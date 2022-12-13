5090f36f890fd396ca764d7e502a50d9e15479e4
zhengqi zhang <770166635@qq.com>
2022-12-13 17:40:18 +0800

    add markdown navbar

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Home from "@/views/home";
@@ -8,7 +8,14 @@ import BlogDetail from "@/views/blog/detail";
 import ScrollToTop from "@/hooks/useScrollToTop";
 import IframeEmbedding from "@/components/IframeEmbedding";
 
+const hiddenFooterRoutesKeywords = ["blog"];
+
 function Homepage() {
+  const location = useLocation();
+  const HiddenFooter = hiddenFooterRoutesKeywords.some(
+    (keyword) => ~location.pathname.indexOf(keyword)
+  );
+
   return (
     <ScrollToTop>
       <div className="App bg-white min-h-[100vh]">
@@ -29,7 +36,7 @@ function Homepage() {
             }
           ></Route>
         </Routes>
-        <Footer />
+        {HiddenFooter ? null : <Footer />}
       </div>
     </ScrollToTop>
   );
