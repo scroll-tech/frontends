b29fd6f98be62b8e50a88f6714dbb650412261cc
zhengqi zhang <770166635@qq.com>
2022-12-16 12:41:38 +0800

    add Iframe Loading status

--- src/components/IframeEmbedding.tsx
+++ src/components/IframeEmbedding.tsx
@@ -1,7 +1,6 @@
 import { styled } from "@mui/system";
 import { Box, CircularProgress } from "@mui/material";
-import { useRef } from "react";
-import { useIsIFrameLoaded } from "@/hooks/useIsIframeLoaded";
+import { useEffect, useRef, useState } from "react";
 
 const Iframe = styled("iframe")(({ theme }) => ({}));
 
@@ -11,11 +10,20 @@ function IframeEmbedding(props: {
   MobileHeight: string;
 }) {
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const isIFrameLoaded = useIsIFrameLoaded(iframeRef);
+  const [isIFrameLoaded, setIsIFrameLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    const iframeCurrent = iframeRef.current;
+    iframeCurrent?.addEventListener("load", () => {
+      setIsIFrameLoaded(true);
+    });
+    return () => {
+      iframeCurrent?.removeEventListener("load", () => setIsIFrameLoaded(true));
+    };
+  }, [iframeRef]);
 
   return (
     <Box>
-      {isIFrameLoaded ? (
+      {!isIFrameLoaded ? (
         <Box
           display="flex"
           justifyContent="center"
@@ -24,22 +32,21 @@ function IframeEmbedding(props: {
         >
           <CircularProgress sx={{ color: "#EB7106" }} />
         </Box>
-      ) : (
-        <Iframe
-          title="url"
-          className="w-full"
-          id="Iframe"
-          seamless
-          src={props.url}
-          ref={iframeRef}
-          sx={{
-            height: {
-              md: props.DesktopHeight,
-              xs: props.MobileHeight,
-            },
-          }}
-        ></Iframe>
-      )}
+      ) : null}
+      <Iframe
+        title="url"
+        className="w-full"
+        seamless
+        src={props.url}
+        ref={iframeRef}
+        sx={{
+          display: isIFrameLoaded ? "block" : "none",
+          height: {
+            md: props.DesktopHeight,
+            xs: props.MobileHeight,
+          },
+        }}
+      ></Iframe>
     </Box>
   );
 }
