18af785ee0a2978f358d69c7e8deae8b335b13df
holybasil <holybasil.1128@gmail.com>
2022-12-20 13:57:22 +0800

    fix: remove extra space

--- src/pages/faucet/components/faq.tsx
+++ src/pages/faucet/components/faq.tsx
@@ -16,13 +16,13 @@ const FAQ = () => {
   return (
     <Faq>
       <FaqItem title="What is a testnet faucet?">
-        <p className="text-[16px] leading-[26px] pb-[40px] text-[#595959] max-w-[680px]">
+        <p className="text-[16px] leading-[26px] text-[#595959] max-w-[680px]">
           Testnets act like a sandbox, where developers can test out features before the mainnet launch. Users provide their wallet address and in
           exchange receive ‘’test tokens’’ (no real world value), to interact with the testnet and share feedback.
         </p>
       </FaqItem>
       <FaqItem title="How do I use the faucet?">
-        <div className="text-[16px] leading-[26px] pb-[40px] text-[#595959]">
+        <div className="text-[16px] leading-[26px] text-[#595959]">
           <ul className="pl-[20px] list-decimal">
             <li>
               <span onClick={() => addToMetaMask(Addresses[ChainId.SCROLL_LAYER_1].autoconnect)} className="link-button cursor-pointer">
