5440295dbb94386b482a0e9b92c82a8d22f11a59
zhengqi zhang <770166635@qq.com>
2023-01-04 11:17:50 +0800

    update token list subscription address

--- src/pages/swap/constants/lists.ts
+++ src/pages/swap/constants/lists.ts
@@ -1,4 +1,6 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL = "tokens.uniswap.eth"
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [window.location.origin + "/swap/token-list-" + process.env.REACT_APP_SCROLL_ENVIRONMENT + ".json"]
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  `https://raw.githubusercontent.com/scroll-tech/token-list/${process.env.REACT_APP_SCROLL_ENVIRONMENT?.toLocaleLowerCase()}/scroll.tokenlist.json`,
+]
