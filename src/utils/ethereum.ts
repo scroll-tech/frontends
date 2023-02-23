c9e5ba85e03567b1a1f74a4acd2f8b236ed4226f
zhengqi zhang <770166635@qq.com>
2023-02-23 19:50:40 +0800

    not add new network if it exist

--- src/utils/ethereum.ts
+++ src/utils/ethereum.ts
@@ -1,8 +1,16 @@
 import { addresses } from "@/constants"
 
-export const switchNetwork = (chainId: number) => {
-  return window.ethereum.request({
-    method: "wallet_addEthereumChain",
-    params: [addresses.find(address => address.chainIdDec === chainId)!.autoconnect],
-  })
+export const switchNetwork = async (chainId: number) => {
+  try {
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x" + chainId.toString() }],
+    })
+    return true
+  } catch (error) {
+    return window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [addresses.find(address => address.chainIdDec === chainId)!.autoconnect],
+    })
+  }
 }
