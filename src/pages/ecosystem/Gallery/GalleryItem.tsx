b4ab3d6b57d9954051e79a92606fb24902237605
holybasil <holybasil.1128@gmail.com>
2023-04-14 10:40:21 +0800

    fix: bigger and farther link icons

--- src/pages/ecosystem/Gallery/GalleryItem.tsx
+++ src/pages/ecosystem/Gallery/GalleryItem.tsx
@@ -40,7 +40,7 @@ const FaceSide = styled(motion.div)(
   backface-visibility: hidden;
   box-shadow: 2px 2px 10px 2px rgba(221, 221, 221, 0.5);
   background-color: rgba(249, 249, 249, 0.3);
-  padding: 1rem;
+  padding: 1.6rem;
   border-radius: 2rem;
   background-color: #fff;
   .MuiAvatar-img{
@@ -129,13 +129,13 @@ const GalleryItem = props => {
             >
               {desc}
             </Typography>
-            <Stack direction="row" spacing={0.5} justifyContent="center" sx={{ width: "100%" }}>
+            <Stack direction="row" spacing={1} justifyContent="flex-end" sx={{ width: "100%" }}>
               {socialLinks.map(social => (
                 <motion.span key={social.name} whileHover={{ scale: 1.1, color: "#686868" }} style={{ color: "#404040" }}>
                   <SvgIcon
                     onClick={e => handleOpenTab(e, social, { website, twitterHandle })}
                     component={social.icon}
-                    sx={{ width: "2rem", height: "2rem" }}
+                    sx={{ width: "2.2rem", height: "2.2rem", verticalAlign: "middle" }}
                     viewBox={social.viewBox}
                   ></SvgIcon>
                 </motion.span>
