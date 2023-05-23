d8d878324108f013ab661045b7c85e2fb0c2a74f
zhengqi zhang <770166635@qq.com>
2023-05-23 19:21:04 +0800

    hidden blue bar

--- src/components/Header/desktop_header.tsx
+++ src/components/Header/desktop_header.tsx
@@ -8,7 +8,7 @@ import { styled } from "@mui/system"
 import Logo from "@/components/Logo"
 import { medias } from "@/constants/medias"
 
-import Announcement from "./announcement"
+// import Announcement from "./announcement"
 import { homeNavigations, navigations } from "./constants"
 
 const StyledBox = styled(Stack)(
@@ -246,7 +246,7 @@ const App = props => {
 
   return (
     <StyledBox>
-      <Announcement />
+      {/* <Announcement /> */}
 
       <HeaderContainer>
         <NavLink to="/" className="flex">
