ef90f76336dd008ebed9027514fb2fea23c2f4b7
holybasil <holybasil.1128@gmail.com>
2023-06-13 16:13:44 +0800

    fix: support load more

--- src/apis/ecosystem.ts
+++ src/apis/ecosystem.ts
@@ -1,3 +1,3 @@
 export const ecosystemOrigin = "https://scroll-eco-list.netlify.app"
-export const ecosystemListHashUrl = ecosystemOrigin + "/docs/ecosystem.hash.json"
+export const ecosystemListHashUrl = ecosystemOrigin + "/docs/"
 export const ecosystemListLogoUrl = ecosystemOrigin + "/logos/"
