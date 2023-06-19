c256ab1056267660d65f67c94e7235ab70418cf4
Holybasil <holybasil.1128@gmail.com>
2023-06-19 09:02:55 +0800

    fix (ecosystem): support for 'load more' (#393)
    * fix: support load more
    
    * fix: render if visible

--- src/pages/ecosystem/Gallery/GalleryItem.tsx
+++ src/pages/ecosystem/Gallery/GalleryItem.tsx
@@ -6,6 +6,9 @@ import { InfoOutlined, ReplayOutlined } from "@mui/icons-material"
 import { Box, Stack, SvgIcon, Typography } from "@mui/material"
 import { styled } from "@mui/material/styles"
 
+import { ecosystemListLogoUrl } from "@/apis/ecosystem"
+import RenderIfVisible from "@/components/RenderIfVisible"
+
 import { socialLinks } from "../helper"
 
 const Wrapper = styled(motion.div)(
@@ -87,11 +90,9 @@ const variants = {
 
 const GalleryItem = props => {
   const {
-    logoBaseUrl,
     item: { name, hash, ext, tags, desc, website, twitterHandle },
   } = props
-
-  const logo = logoBaseUrl + name + ext
+  const logo = ecosystemListLogoUrl + name + ext
 
   const [isBack, setIsBack] = useState(false)
 
@@ -109,61 +110,70 @@ const GalleryItem = props => {
     window.open(website)
   }
   return (
-    <Wrapper onClick={handleFlipCard} whileHover={{ translateY: "-2px", scale: 1.005 }}>
-      <FlipCard animate={isBack ? "back" : "front"} variants={variants} transition={{ duration: 0.3, ease: "easeInOut" }}>
-        <FaceSide
-          className="front"
-          whileHover={{ boxShadow: "2px 2px 10px 2px rgba(131, 131, 131, 0.5)" }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-        >
-          <IconBox sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <InfoOutlined sx={{ color: "#686868" }}></InfoOutlined>
-          </IconBox>
-          <Stack direction="column" spacing={2} alignItems="center" sx={{ mt: "7rem" }}>
-            <Stack direction="row" spacing={1.25} alignItems="center">
-              <Img alt={name} src={logo} placeholder={hash} width={84} height={84}></Img>
-              <Typography sx={{ fontFamily: "Inter", fontWeight: 600, fontSize: ["2rem", "2.4rem"], width: "min-content" }}>{name}</Typography>
-            </Stack>
-            <Stack direction="row" sx={{ flexWrap: "wrap", justifyContent: "center" }}>
-              {tags ? tags.map(value => <Tag key={value}>{value.trim()}</Tag>) : null}
-            </Stack>
-          </Stack>
-        </FaceSide>
-        <FaceSide
-          className="back"
-          whileHover={{ boxShadow: "2px 2px 10px 2px rgba(131, 131, 131, 0.5)" }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-          style={{ transform: "rotateY(180deg)" }}
-        >
-          <Stack direction="column" justifyContent="space-between" sx={{ height: "100%" }}>
-            <Stack direction="row" justifyContent="space-between">
-              <Stack direction="row" alignItems="center" spacing={0.5}>
-                <Img alt={name} src={logo} placeholder={hash} width={22} height={22}></Img>
-                <Typography sx={{ fontWeight: 600, fontSize: 12 }}>{name}</Typography>
+    <RenderIfVisible defaultHeight={340}>
+      <Wrapper onClick={handleFlipCard} whileHover={{ translateY: "-2px", scale: 1.005 }}>
+        <FlipCard animate={isBack ? "back" : "front"} variants={variants} transition={{ duration: 0.3, ease: "easeInOut" }}>
+          <FaceSide
+            className="front"
+            whileHover={{ boxShadow: "2px 2px 10px 2px rgba(131, 131, 131, 0.5)" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <IconBox sx={{ display: "flex", justifyContent: "flex-end" }}>
+              <InfoOutlined sx={{ color: "#686868" }}></InfoOutlined>
+            </IconBox>
+            <Stack direction="column" spacing={2} alignItems="center" sx={{ mt: "7rem" }}>
+              <Stack direction="row" spacing={1.25} alignItems="center">
+                <Img alt={name} src={logo} placeholder={hash} width={84} height={84}></Img>
+                <Typography sx={{ fontFamily: "Inter", fontWeight: 600, fontSize: ["2rem", "2.4rem"], width: "min-content" }}>{name}</Typography>
               </Stack>
-              <ReplayOutlined sx={{ color: "#686868" }}></ReplayOutlined>
+              <Stack direction="row" sx={{ flexWrap: "wrap", justifyContent: "center" }}>
+                {tags ? tags.map(value => <Tag key={value}>{value.trim()}</Tag>) : null}
+              </Stack>
             </Stack>
-            <Typography
-              sx={{ mb: "1rem", px: "1rem", lineHeight: ["1.8rem", "1.6rem"], fontFamily: "Inter", fontWeight: 500, fontSize: ["1.6rem", "1.4rem"] }}
-            >
-              {desc}
-            </Typography>
-            <Stack direction="row" spacing={[1, 0.5]} justifyContent="flex-end" sx={{ width: "100%" }}>
-              {socialLinks.map(social => (
-                <motion.span key={social.name} whileHover={{ scale: 1.1, color: "#686868" }} style={{ color: "#404040" }}>
-                  <SvgIcon
-                    onClick={e => handleOpenTab(e, social, { website, twitterHandle })}
-                    component={social.icon}
-                    sx={{ width: ["2.2rem", "2rem"], height: ["2.2rem", "2rem"], verticalAlign: "middle" }}
-                    inheritViewBox
-                  ></SvgIcon>
-                </motion.span>
-              ))}
+          </FaceSide>
+          <FaceSide
+            className="back"
+            whileHover={{ boxShadow: "2px 2px 10px 2px rgba(131, 131, 131, 0.5)" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            style={{ transform: "rotateY(180deg)" }}
+          >
+            <Stack direction="column" justifyContent="space-between" sx={{ height: "100%" }}>
+              <Stack direction="row" justifyContent="space-between">
+                <Stack direction="row" alignItems="center" spacing={0.5}>
+                  <Img alt={name} src={logo} placeholder={hash} width={22} height={22}></Img>
+                  <Typography sx={{ fontWeight: 600, fontSize: 12 }}>{name}</Typography>
+                </Stack>
+                <ReplayOutlined sx={{ color: "#686868" }}></ReplayOutlined>
+              </Stack>
+              <Typography
+                sx={{
+                  mb: "1rem",
+                  px: "1rem",
+                  lineHeight: ["1.8rem", "1.6rem"],
+                  fontFamily: "Inter",
+                  fontWeight: 500,
+                  fontSize: ["1.6rem", "1.4rem"],
+                }}
+              >
+                {desc}
+              </Typography>
+              <Stack direction="row" spacing={[1, 0.5]} justifyContent="flex-end" sx={{ width: "100%" }}>
+                {socialLinks.map(social => (
+                  <motion.span key={social.name} whileHover={{ scale: 1.1, color: "#686868" }} style={{ color: "#404040" }}>
+                    <SvgIcon
+                      onClick={e => handleOpenTab(e, social, { website, twitterHandle })}
+                      component={social.icon}
+                      sx={{ width: ["2.2rem", "2rem"], height: ["2.2rem", "2rem"], verticalAlign: "middle" }}
+                      inheritViewBox
+                    ></SvgIcon>
+                  </motion.span>
+                ))}
+              </Stack>
             </Stack>
-          </Stack>
-        </FaceSide>
-      </FlipCard>
-    </Wrapper>
+          </FaceSide>
+        </FlipCard>
+      </Wrapper>
+    </RenderIfVisible>
   )
 }
 
--- src/pages/ecosystem/Gallery/index.tsx
+++ src/pages/ecosystem/Gallery/index.tsx
@@ -1,10 +1,10 @@
-import { useMemo, useState } from "react"
-import useSWR from "swr"
+import { useEffect, useMemo, useState } from "react"
 
-import { Alert, Snackbar } from "@mui/material"
+import { Alert, Box, Snackbar } from "@mui/material"
 import { styled } from "@mui/material/styles"
 
-import { ecosystemListHashUrl, ecosystemListLogoUrl } from "@/apis/ecosystem"
+import { ecosystemListHashUrl } from "@/apis/ecosystem"
+import LoadingButton from "@/components/LoadingButton"
 import LoadingPage from "@/components/LoadingPage"
 import { DIVERGENT_CATEGORY_MAP } from "@/constants"
 
@@ -15,17 +15,17 @@ const Container = styled("div")(
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   width: max-content;
-  margin: 3rem auto 8rem;
+  margin: 3rem auto;
   grid-auto-columns: 1fr;
   ${theme.breakpoints.down("lg")} {
     grid-template-columns: repeat(2, 1fr);
     width: max-content;
-    margin: 2rem auto 4rem;
+    margin: 2rem auto;
   };
   ${theme.breakpoints.down("sm")} {
     grid-template-columns: 1fr;
     width: 100%;
-    margin: 2rem auto 2.4rem;
+    margin: 2rem auto;
   };
 `,
 )
@@ -33,39 +33,80 @@ const Container = styled("div")(
 const Gallery = props => {
   const { selectedCategory } = props
 
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [ecosystemList, setEcosystemList] = useState([])
   const [errorMsg, setErrorMsg] = useState("")
-  const { data: ecosystemList, isLoading } = useSWR(ecosystemListHashUrl, url => {
-    return scrollRequest(url).catch(() => {
-      setErrorMsg("Fail to fetch ecosystem list")
-      return null
-    })
-  })
+
+  useEffect(() => {
+    setLoading(true)
+    scrollRequest(`${ecosystemListHashUrl}ecosystem.hash.p${page}.json`)
+      .then(data => {
+        setEcosystemList(pre => pre.concat(data))
+      })
+      .catch(() => {
+        setErrorMsg("Fail to fetch ecosystem list")
+        return null
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [page])
+
+  // TODO: for test, remove later
+  // const hasMore = true
+  // useEffect(() => {
+  //   if ((ecosystemList.slice(-1)[0] as any)?.isLastOne) {
+  //     setPage(1)
+  //   }
+  // }, [ecosystemList])
+
+  const hasMore = useMemo(() => {
+    if (!ecosystemList.length) {
+      return false
+    }
+    const lastOne: any = ecosystemList.slice(-1)[0]
+    return !lastOne.isLastOne
+  }, [ecosystemList])
 
   const filteredEcosystemList = useMemo(() => {
     if (selectedCategory === "All") {
       return ecosystemList
     }
-    return ecosystemList.filter(item => item.tags.some(item => DIVERGENT_CATEGORY_MAP[selectedCategory].includes(item)))
+    return ecosystemList.filter((item: any) => item.tags.some(item => DIVERGENT_CATEGORY_MAP[selectedCategory].includes(item)))
   }, [ecosystemList, selectedCategory])
 
   const handleClose = () => {
     setErrorMsg("")
   }
+
+  const handleLoadNextPage = () => {
+    setPage(pre => pre + 1)
+  }
   return (
     <>
-      {isLoading ? (
+      {loading && !ecosystemList.length ? (
         <LoadingPage height="60vh"></LoadingPage>
       ) : (
-        <Container>
-          {filteredEcosystemList?.map(item => (
-            <GalleryItem key={item.name} logoBaseUrl={ecosystemListLogoUrl} item={item}></GalleryItem>
-          ))}
-          <Snackbar open={!!errorMsg} autoHideDuration={6000} onClose={handleClose}>
-            <Alert severity="error" onClose={handleClose}>
-              {errorMsg}
-            </Alert>
-          </Snackbar>
-        </Container>
+        <>
+          <Container>
+            {filteredEcosystemList?.map((item: any) => (
+              <GalleryItem key={item.name} item={item}></GalleryItem>
+            ))}
+            <Snackbar open={!!errorMsg} autoHideDuration={6000} onClose={handleClose}>
+              <Alert severity="error" onClose={handleClose}>
+                {errorMsg}
+              </Alert>
+            </Snackbar>
+          </Container>
+          {hasMore && (
+            <Box sx={{ textAlign: "center", mb: ["2rem", "3rem", "5rem"] }}>
+              <LoadingButton sx={{ width: "20rem" }} variant="contained" loading={loading && page > 1} onClick={handleLoadNextPage}>
+                Load More
+              </LoadingButton>
+            </Box>
+          )}
+        </>
       )}
     </>
   )
