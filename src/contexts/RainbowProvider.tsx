09a87851d3e0587caeedf43a25b345ea75bfaff6
holybasil <holybasil.1128@gmail.com>
2023-06-26 17:16:50 +0800

    fix: reset explorerRecommendedWalletIds

--- src/contexts/RainbowProvider.tsx
+++ src/contexts/RainbowProvider.tsx
@@ -81,6 +81,16 @@ const connectors = connectorsForWallets([
             url: "https://scroll.io/",
             icons: ["https://scroll.io/logo.png"],
           },
+          qrModalOptions: {
+            explorerRecommendedWalletIds: [
+              // metamask
+              "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+              // trust
+              "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
+              // uniswap
+              "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a",
+            ],
+          },
         },
       } as any),
     ],
