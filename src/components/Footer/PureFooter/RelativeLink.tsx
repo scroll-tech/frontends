79e9dd68a11521cc4bc2229f140701119ef3d0a3
holybasil <holybasil.1128@gmail.com>
2023-07-17 14:35:18 +0800

    fix: mobile footer

--- src/components/Footer/PureFooter/RelativeLink.tsx
+++ src/components/Footer/PureFooter/RelativeLink.tsx
@@ -1,16 +1,27 @@
+import { isMobile } from "react-device-detect"
 import { Link } from "react-router-dom"
 
 import { Stack, SvgIcon, Typography } from "@mui/material"
-import useMediaQuery from "@mui/material/useMediaQuery"
 
 import { ReactComponent as ArrowIcon } from "@/assets/svgs/footer/arrow.svg"
 
 const LinkText = props => {
-  const matches = useMediaQuery((theme: any) => theme.breakpoints.down("md"))
   return (
     <Stack direction="row" alignItems="center" spacing={0.5}>
-      <Typography sx={{ color: "#FFF8F3", fontSize: "1.5rem", lineHeight: "4rem", cursor: "pointer" }}>{props.children}</Typography>
-      {matches && <SvgIcon component={ArrowIcon} inheritViewBox sx={{ fontSize: "0.6rem", verticalAlign: "middle" }}></SvgIcon>}
+      <Typography
+        sx={{
+          color: "#FFF8F3",
+          fontSize: ["1.6rem", "1.5rem"],
+          lineHeight: ["4rem", "4.5rem"],
+          cursor: "pointer",
+          "&:hover": {
+            color: theme => theme.palette.primary.dark,
+          },
+        }}
+      >
+        {props.children}
+      </Typography>
+      {isMobile && <SvgIcon component={ArrowIcon} inheritViewBox sx={{ fontSize: "0.6rem", verticalAlign: "middle" }}></SvgIcon>}
     </Stack>
   )
 }
