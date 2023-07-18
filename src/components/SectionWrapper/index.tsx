988e7de85f16f20868518a00635ac6ae6ac6f29e
holybasil <holybasil.1128@gmail.com>
2023-07-18 18:41:54 +0800

    fix: full in footer

--- src/components/SectionWrapper/index.tsx
+++ src/components/SectionWrapper/index.tsx
@@ -4,7 +4,7 @@ const SectionWrapper = props => {
   const { dark, children, sx, full, maxWidth = "130rem", ...restProps } = props
   return (
     <Box sx={{ backgroundColor: theme => (dark ? theme.palette.themeBackground.dark : theme.palette.themeBackground.light) }} {...restProps}>
-      <Container sx={{ pt: "15.4rem", maxWidth: full ? "unset" : `${maxWidth} !important`, ...sx }}>{children}</Container>
+      <Container sx={{ pt: "15.4rem", maxWidth: full ? "unset !important" : `${maxWidth} !important`, ...sx }}>{children}</Container>
     </Box>
   )
 }
