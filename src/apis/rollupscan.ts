64a473f2d5c9246503a8794c1bcebf2f06056e94
zhengqi zhang <770166635@qq.com>
2023-07-30 10:51:36 +0800

    Support new API endpoints

--- src/apis/rollupscan.ts
+++ src/apis/rollupscan.ts
@@ -1,21 +1,19 @@
 import { requireEnv } from "@/utils"
 
-const baseUrl = requireEnv("REACT_APP_API_BASE_URI")
+const baseUrl = requireEnv("REACT_APP_ROLLUPSCAN_API_URI")
 
-const prefix = requireEnv("REACT_APP_ROLLUPSCAN_API_PREFIX")
+export const fetchLastBatchIndexesUrl = `${baseUrl}/last_batch_indexes`
 
-export const fetchLastBatchIndexesUrl = `${baseUrl}${prefix}/last_batch_indexes`
+export const fetchBatchListUrl = `${baseUrl}/batches`
 
-export const fetchBatchListUrl = `${baseUrl}${prefix}/batches`
+export const fetchBatchDetailUrl = `${baseUrl}/batch`
 
-export const fetchBatchDetailUrl = `${baseUrl}${prefix}/batch`
+export const fetchChunkListUrl = `${baseUrl}/chunks`
 
-export const fetchChunkListUrl = `${baseUrl}${prefix}/chunks`
+export const fetchChunkBlocksUrl = `${baseUrl}/chunk_blocks`
 
-export const fetchChunkBlocksUrl = `${baseUrl}${prefix}/chunk_blocks`
+export const fetchBatchBlocksUrl = `${baseUrl}/batch_blocks`
 
-export const fetchBatchBlocksUrl = `${baseUrl}${prefix}/batch_blocks`
+export const searchUrl = `${baseUrl}/search`
 
-export const searchUrl = `${baseUrl}${prefix}/search`
-
-export const fetchBetchDetail = `${baseUrl}${prefix}/batch`
+export const fetchBetchDetail = `${baseUrl}/batch`
