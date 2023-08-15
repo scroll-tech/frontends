70d44331030bdb60e0abe12b3ca05534eea044cf
zhengqi zhang <770166635@qq.com>
2023-08-15 20:11:08 +0800

    rebrand blog page

--- src/components/ArticleCard/index.tsx
+++ src/components/ArticleCard/index.tsx
@@ -1,85 +1,62 @@
 import { useNavigate } from "react-router-dom"
 
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { styled } from "@mui/system"
 
 import WebpImage from "@/components/WebpImage"
 
-const ArticleInfo = styled(Box)(
-  ({ theme }) => `
-    max-width: 40rem;
-    &.small {
-      padding: 0 2rem 4rem;
-      ${theme.breakpoints.down("md")} {
-        height: 34rem;
-      };
-  }
-  `,
-)
+const ArticleInfo = styled(Box)(({ theme }) => ({}))
 
-const ArticleTitle = styled(Typography)(
-  ({ theme }) => `
-        color: ${theme.palette.text.primary};
-        font-weight: 600;
-        cursor: pointer;
-        line-height: 2.8rem;
-        display: table-cell;
-        vertical-align: bottom;
-        &.small {
-          height: 10rem;
-        }
-        `,
-)
+const ArticleTitle = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  fontWeight: 700,
+  cursor: "pointer",
+  lineHeight: "2.8rem",
+  display: "table-cell",
+  verticalAlign: "bottom",
+  marginBottom: "0.6rem",
+}))
 
-const Card = styled(Box)(
-  ({ theme }) => `
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        max-width: 84rem;
-        text-align: left;
-        margin: 0 auto;
-        overflow: hidden;
-        &.small {
-          max-width: 38rem;
-          flex-direction: column;
-          box-shadow: rgb(0 0 0 / 15%) 5px 5px 3px;
-          border-radius: ${theme.shape.borderRadius}px;
-        }
-    `,
-)
+const Card = styled(Box)(({ theme }) => ({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gap: "3rem",
+  textAlign: "left",
+  margin: "0 auto",
+  overflow: "hidden",
+  "&.small": {
+    maxWidth: "38rem",
+    flexDirection: "column",
+    boxShadow: "rgb(0 0 0 / 15%) 5px 5px 3px",
+    borderRadius: `${theme.shape.borderRadius}px`,
+  },
+}))
 
-const ArticleDate = styled(Typography)(
-  ({ theme }) => `
-  margin-bottom: 0.5rem;
-          `,
-)
+const ArticleDate = styled(Typography)({
+  marginBottom: "0.5rem",
+  fontSize: "1.4rem",
+  fontWeight: 500,
+})
 
-const ArticleSummary = styled(Typography)(
-  ({ theme }) => `
-    margin-bottom: 2.2rem;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-            `,
-)
+const ArticleSummary = styled(Typography)({
+  marginBottom: "2.2rem",
+  overflow: "hidden",
+  maxWidth: "44rem",
+  textOverflow: "ellipsis",
+  display: "-webkit-box",
+  "-webkit-line-clamp": 2,
+  "-webkit-box-orient": "vertical",
+})
 
-const ArticlePoster = styled(WebpImage)(
-  ({ theme }) => `
-      width: 38rem;
-      height: 23.3rem;
-      cursor: pointer;
-      ${theme.breakpoints.up("lg")} {
-        border-radius: ${theme.shape.borderRadius}px;
-      };
-    `,
-)
+const ArticlePoster = styled(WebpImage)(({ theme }) => ({
+  width: "38rem",
+  height: "23.3rem",
+  cursor: "pointer",
+  borderRadius: "2.5rem",
+}))
 
 const ArticleCard = ({ blog, small = false }) => {
   const navigate = useNavigate()
-
   const handleClick = () => {
     if (blog.externalLink) {
       window.location.href = blog.externalLink
@@ -89,22 +66,17 @@ const ArticleCard = ({ blog, small = false }) => {
   }
 
   return (
-    <Card className={small ? "small" : ""}>
-      <ArticlePoster src={blog.posterImg} webpsrc={blog.posterImg.replace(".jpg", ".webp")} onClick={handleClick}></ArticlePoster>
-      <ArticleInfo className={small ? "small" : ""}>
-        <ArticleTitle className={small ? "small" : ""} onClick={handleClick} variant="subtitle1">
+    <Card>
+      <ArticleInfo>
+        <ArticleTitle onClick={handleClick} variant="H4">
           {blog.title}
         </ArticleTitle>
-        <ArticleDate className={small ? "small" : ""} variant="body2" color="textSecondary">
+        <ArticleSummary variant="Body3">{blog.summary}</ArticleSummary>
+        <ArticleDate variant="body2">
           {blog.date} ・ {blog.type}
         </ArticleDate>
-        <ArticleSummary className={small ? "small" : ""} variant="body1" color="textSecondary">
-          {blog.summary}
-        </ArticleSummary>
-        <Button className={small ? "small" : ""} onClick={handleClick} color="secondary">
-          Read More
-        </Button>
       </ArticleInfo>
+      <ArticlePoster src={blog.posterImg} webpsrc={blog.posterImg.replace(".jpg", ".webp")} onClick={handleClick}></ArticlePoster>
     </Card>
   )
 }
