201556efcd7e0d2c38bce9ac0c5768cf0960f607
holybasil <holybasil.1128@gmail.com>
2023-08-15 15:57:27 +0800

    fix: story page copy and icons

--- src/pages/ourStory/JoinTeam/index.tsx
+++ src/pages/ourStory/JoinTeam/index.tsx
@@ -8,8 +8,8 @@ const JoinTeam = () => {
     <SectionWrapper round full sx={{ pt: "15.4rem", pb: "16rem", backgroundColor: "themeBackground.normal" }}>
       <Stack direction="column" spacing="5rem" alignItems="center">
         <Typography sx={{ fontSize: ["3.2rem", "4.8rem"], fontWeight: 500 }}>Join the Scroll team!</Typography>
-        <Button href="/join-us" color="primary" width="18rem">
-          Career
+        <Button href="/join-us" color="primary">
+          Careers
         </Button>
       </Stack>
     </SectionWrapper>
