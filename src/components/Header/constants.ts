29264882baf574df518ddae08c187e67a84bb236
zhengqi zhang <770166635@qq.com>
2023-08-16 13:02:14 +0800

    update nav path

--- src/components/Header/constants.ts
+++ src/components/Header/constants.ts
@@ -37,7 +37,7 @@ const navigations = [
           {
             label: "Join our Team ",
             key: "join our team",
-            href: "/join-our-team",
+            href: "/join-us",
           },
           {
             label: "Events",
