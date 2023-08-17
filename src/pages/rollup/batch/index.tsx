454fa2615d931c54db4c487c5d80298eb26be924
zhengqi zhang <770166635@qq.com>
2023-08-17 16:51:39 +0800

    rebranding rollupscan style

--- src/pages/rollup/batch/index.tsx
+++ src/pages/rollup/batch/index.tsx
@@ -51,6 +51,7 @@ const StatusChip = styled(Chip)(({ theme }) => ({
 const LabelTypography = styled(Typography)(({ theme }) => ({
   paddingLeft: "5rem",
   width: "30rem",
+  fontWeight: 500,
   [theme.breakpoints.down("md")]: {
     paddingLeft: "1.6rem",
     width: "25rem",
@@ -141,7 +142,7 @@ const Batch = () => {
                 sx={{
                   width: "100%",
                   border: theme => `1px solid ${theme.palette.border.main}`,
-                  borderRadius: "10px",
+                  borderRadius: "27px",
                   marginTop: "2.2rem",
                 }}
                 aria-label="batch"
--- src/pages/rollup/components/TableCell.tsx
+++ src/pages/rollup/components/TableCell.tsx
@@ -2,19 +2,19 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell"
 import { styled } from "@mui/material/styles"
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  fontWeight: "500",
+  color: theme.palette.text.primary,
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.scaleBackground.secondary,
-    color: theme.palette.text.secondary,
-    fontWeight: "500",
-    fontSize: 14,
+    fontSize: 16,
     height: "6rem",
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: "1.6rem",
     height: "10rem",
+    fontWeight: "500",
   },
   "&:nth-of-type(1)": {
-    paddingLeft: "5rem",
+    paddingLeft: "2.7rem",
   },
 }))
 export default StyledTableCell
