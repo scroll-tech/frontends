9032c672f8e32fef34a59a4ee9e03bcfe3919776
zhengqi zhang <770166635@qq.com>
2023-08-24 18:21:29 +0800

    fix conflicts

--- src/components/Header/constants.ts
+++ src/components/Header/constants.ts
@@ -70,7 +70,6 @@ const navigations = [
     href: "",
     children: [
       {
-        label: "explore1",
         children: [
           {
             label: "Ecosystem",
@@ -79,77 +78,16 @@ const navigations = [
           },
           {
             label: "User Guide",
-            key: "user guide",
-            href: "https://docs.scroll.io/en/user-guide/",
-            isExternal: true,
-          },
-        ],
-      },
-      {
-        label: "Block Explorers",
-        children: [
-          {
-            label: "Rollup Explorer",
-            key: "rollupExplorer",
-            href: "/rollupscan?page=1&per_page=10",
-          },
-          {
-            label: "Scroll Blockscout",
-            key: "l2BlockExplorer",
-            href: EXPLORER_URL.L2,
+            key: "userGuide",
+            href: "https://docs.scroll.xyz/en/user-guide/",
             isExternal: true,
           },
           {
-            label: "Dora Explorer",
-            key: "doraBlockExplorer",
-            href: EXPLORER_URL.Dora,
+            label: "Forum",
+            key: "forum",
+            href: "https://community.scroll.io",
             isExternal: true,
           },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Build",
-    key: "build",
-    href: "",
-    children: [
-      {
-        label: "Build",
-        children: [
-          {
-            label: "Documentation",
-            key: "documentation",
-            href: "https://docs.scroll.io/en/home/",
-            isExternal: true,
-          },
-          {
-            label: "Developer Guide",
-            key: "developer guide",
-            href: "https://docs.scroll.io/en/developers/",
-            isExternal: true,
-          },
-          {
-            label: "Technology",
-            key: "technology",
-            href: "https://docs.scroll.io/en/technology/",
-            isExternal: true,
-          },
-        ],
-      },
-    ],
-  },
-
-          {
-            label: "Blog",
-            key: "blog",
-            href: "/blog",
-          },
-        ],
-      },
-      {
-        label: "Explore2",
-        children: [
           {
             label: "Blog",
             key: "blog",
