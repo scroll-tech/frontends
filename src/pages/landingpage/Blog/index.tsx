528adc4ea6e43ba240bac610995c181049b037c6
zhengqi zhang <770166635@qq.com>
2023-08-24 12:18:42 +0800

    update landingpage blog

--- src/pages/landingpage/Blog/index.tsx
+++ src/pages/landingpage/Blog/index.tsx
@@ -5,37 +5,43 @@ import Button from "@/components/Button"
 import SuccessionToView, { SuccessionItem } from "@/components/Motion/SuccessionToView"
 import SectionHeader from "@/components/SectionHeader"
 import SectionWrapper from "@/components/SectionWrapper"
-
-import StoryCard from "./StoryCard"
+import StoryCard from "@/pages/ourStory/BuildingStory/StoryCard"
 
 const STORIES = [
   {
-    cover: "imgs/story/story-blog-cover.png",
-    title: "Our philosophy on zk...",
+    cover: "/imgs/homepage/blog-cover-1.png",
+    title: "Build with Scroll: Quickstart Guide",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada. consectetur adipiscing elit. Donec dictum auctor sem et malesuada",
+      "Check out our documentation and start developing and deploying on Scroll right away with all of your favorite tools for building and testing smart contracts.",
+    href: "https://docs.scroll.xyz/en/home/",
   },
   {
-    title: "Podcast title here",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada.",
+    title: "Scroll’s Fresh Coat: A Community Touch",
+    content: "Discover our updated website and developer portal, shaped with community insights and collaboration.",
+    href: "",
   },
   {
-    title: "News article here",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada.",
+    title: "Announcing the Scroll Beta Testnet on Sepolia",
+    content: "Learn about the infastructure updates and improvements in our Beta Testnet as we enter the final phase before Mainnet.",
+    href: "/blog/alphaTestnet",
   },
   {
-    title: "KOL blog here",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada.",
-  },
-  {
-    title: "Founder blog here",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada.",
-  },
-  {
-    cover: "imgs/story/story-blog-cover.png",
-    title: "We <3 our devs...",
+    title: "Announcing the 'Contribute to Scroll' Initiative",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum auctor sem et malesuada.",
+      "Open-source contributions are one of the most important cornerstones of the digital age, and at Scroll, we firmly believe in the power of community and are thrilled to introduce our initiative.",
+    href: "/blog/contributeToScroll",
+  },
+  {
+    title: "Scroll’s Architecture Overview",
+    content:
+      "This post provides insights into Scroll's architecture evolution, highlighting the transformation of our zkEVM into a comprehensive zkRollup solution for Ethereum.",
+    href: "/blog/architecture",
+  },
+  {
+    cover: "/imgs/homepage/blog-cover-2.png",
+    title: "Join Scroll at ETH Warsaw",
+    content: "Scroll will be in Warsaw, Poland, from Aug 31-Sept 3. Come say hi and grab some new swag!",
+    href: "https://www.notion.so/scrollzkp/cc9ae9da3d894a90937e04818542e97d?v=a08e1f4656104aefa7b8d3ac829f1c90",
   },
 ]
 
@@ -66,6 +72,7 @@ const useStyles = makeStyles()(theme => ({
 
 const BuildingStory = () => {
   const { classes, cx } = useStyles()
+
   return (
     <SectionWrapper sx={{ pt: ["11rem", "26rem"], pb: ["12rem", "13rem"] }}>
       <SectionHeader
