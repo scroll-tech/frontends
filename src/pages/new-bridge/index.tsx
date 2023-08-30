c8992cebdcffafd8ac9891353e1db7a9ade3d25c
Holybasil <holybasil.1128@gmail.com>
2023-08-30 14:11:16 +0800

    fix: bind txType and fromNetwork (#550)

--- src/pages/new-bridge/index.tsx
+++ src/pages/new-bridge/index.tsx
@@ -1,10 +1,12 @@
 // import createCache from "@emotion/cache"
+import { useEffect } from "react"
 import { isMobileOnly } from "react-device-detect"
 
 import { Stack, Typography } from "@mui/material"
 
 import GlobalWarning from "@/components/GlobalWarning"
 import SectionWrapper from "@/components/SectionWrapper"
+import { NETWORKS } from "@/constants"
 import AppProvider from "@/contexts/AppContextProvider"
 import { PriceFeeProvider } from "@/contexts/PriceFeeProvider"
 import useBridgeStore from "@/stores/bridgeStore"
@@ -16,7 +18,18 @@ import Send from "./Send"
 import TxHistory from "./TxHistory"
 
 const Bridge = () => {
-  const { mode } = useBridgeStore()
+  const { mode, txType, changeFromNetwork, changeToNetwork } = useBridgeStore()
+
+  useEffect(() => {
+    if (txType === "Deposit") {
+      changeFromNetwork(NETWORKS[0])
+      changeToNetwork(NETWORKS[1])
+    } else {
+      changeFromNetwork(NETWORKS[1])
+      changeToNetwork(NETWORKS[0])
+    }
+  }, [txType])
+
   return (
     <AppProvider>
       <PriceFeeProvider>
