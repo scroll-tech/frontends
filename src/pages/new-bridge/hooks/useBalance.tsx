a5a693a6eaa7f940c3cf8cd8532ad11ff0943959
holybasil <holybasil.1128@gmail.com>
2023-08-31 14:39:38 +0800

    test: log data

--- src/pages/new-bridge/hooks/useBalance.tsx
+++ src/pages/new-bridge/hooks/useBalance.tsx
@@ -5,10 +5,10 @@ import { useRainbowContext } from "@/contexts/RainbowProvider"
 const useBalance = tokenAddress => {
   const { walletCurrentAddress } = useRainbowContext()
 
-  const { data, isLoading, error, status } = useBalance_RAW({ address: walletCurrentAddress, token: tokenAddress, watch: true })
+  const { data, isLoading, error } = useBalance_RAW({ address: walletCurrentAddress, token: tokenAddress, watch: true })
 
   console.log(error, "error")
-  console.log(status, "status")
+  console.log(data, "data")
 
   return { balance: data?.value, isLoading }
 }
