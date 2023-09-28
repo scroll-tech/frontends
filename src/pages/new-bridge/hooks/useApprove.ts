6790159fe5107f5f8d90ca73294a4c36440cc62b
zzq0826 <770166635@qq.com>
2023-09-28 12:06:10 +0800

    L2 ERC20 do not need approve (#617)
    * L2 ERC20 do not need approve
    
    * Update judgment statement

--- src/pages/new-bridge/hooks/useApprove.ts
+++ src/pages/new-bridge/hooks/useApprove.ts
@@ -3,6 +3,7 @@ import { useEffect, useMemo, useState } from "react"
 
 import L1_erc20ABI from "@/assets/abis/L1_erc20ABI.json"
 import { GATEWAY_ROUTE_PROXY_ADDR, WETH_GATEWAY_PROXY_ADDR, WETH_SYMBOL } from "@/constants"
+import { CHAIN_ID } from "@/constants"
 import { useApp } from "@/contexts/AppContextProvider"
 import { useRainbowContext } from "@/contexts/RainbowProvider"
 import { amountToBN } from "@/utils"
@@ -22,8 +23,15 @@ const useApprove = (fromNetwork, selectedToken, amount) => {
   }, [fromNetwork, selectedToken])
 
   const tokenInstance = useMemo(() => {
-    if (networksAndSigners[chainId as number]?.signer && !selectedToken.native) {
-      return new ethers.Contract((selectedToken as ERC20Token).address, L1_erc20ABI, networksAndSigners[chainId as number].signer)
+    // ETH & L2 ERC20 don't need approval
+    const hasSigner = networksAndSigners[chainId as number]?.signer
+    const isNativeToken = selectedToken.native
+    const isL2Erc20 = selectedToken.chainId === CHAIN_ID.L2 && selectedToken.symbol !== WETH_SYMBOL
+
+    if (hasSigner && !isNativeToken && !isL2Erc20) {
+      const { address } = selectedToken as ERC20Token
+      const { signer } = networksAndSigners[chainId as number]
+      return new ethers.Contract(address, L1_erc20ABI, signer)
     }
     return null
   }, [selectedToken, networksAndSigners[chainId as number]])
@@ -45,9 +53,6 @@ const useApprove = (fromNetwork, selectedToken, amount) => {
   }
 
   useEffect(() => {
-    if ((selectedToken as NativeToken).native || !tokenInstance) {
-      setIsNeeded(false)
-    }
     checkApproval(selectedToken, amount).then(needApproval => {
       setIsNeeded(needApproval)
     })
