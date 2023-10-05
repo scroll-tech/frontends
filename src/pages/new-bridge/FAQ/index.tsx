10fc3846369dde9a60039be39521027bcee4d81c
stateofkate0 <116920452+stateofkate0@users.noreply.github.com>
2023-10-04 17:02:33 -0700

    ~Changing Bridge FAQ to reflect some copy changes. This is ready for now, will be another version for Mainnet

--- src/pages/new-bridge/FAQ/index.tsx
+++ src/pages/new-bridge/FAQ/index.tsx
@@ -28,25 +28,28 @@ const FAQ = () => {
           blockchains.
         </Typography>
         <Typography variant="Body3" color="textSecondary" className={classes.specialParagraph}>
-          A blockchain bridge is a tool connecting two blockchains to enable interactions between them.
+          A blockchain bridge is a tool which connects two blockchains to enable interactions and move assets between them.
         </Typography>
       </FaqItem>
       <FaqItem title="How to send tokens using the Bridge?" id="3">
-        <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
-          1. Select Deposit or Withdraw.
+      <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
+          1. Switch to the correct network in your wallet. (If you are a first time Scroll user, you will need to add the Scroll network to your wallet. Add network <Link href={SITE_MAP.Home}>here</Link>.)
         </Typography>
         <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
-          2. Select the token (ETH, GHO, or WETH) you wish to transfer.
+          2. Select Deposit or Withdraw on the bridge.
         </Typography>
         <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
-          3. Click on Send. A pop up window from will ask you for the transfer confirmation. If this is your first time transferring ETH, you will be
+          3. Select the token (ETH, GHO, or WETH) you wish to transfer.
+        </Typography>
+        <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
+          4. Click on Send. A pop up window from will ask you for the transfer confirmation. If this is your first time transferring ETH, you will be
           prompted to approve the Scroll Bridge contract to access your ETH token.
         </Typography>
         <Typography variant="Body3" color="textSecondary" className={classes.orderedParagraph}>
-          4. Once the transaction is confirmed, the token will be deducted from your Scroll L1 or Scroll L2 wallet.
+          5. Once the transaction is confirmed, the token will be deducted from your wallet.
         </Typography>
         <Typography variant="Body3" color="textSecondary">
-          More instructions <Link href={SITE_MAP.Home}>here</Link>.
+          More instructions can be found <Link href={SITE_MAP.Home}>here</Link>.
         </Typography>
       </FaqItem>
     </Faq>
