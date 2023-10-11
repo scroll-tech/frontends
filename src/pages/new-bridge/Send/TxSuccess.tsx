3dd5758d6f443881d130a0930ad8e9f24ce8a3bc
Yvaine <holybasil.1128@gmail.com>
2023-10-11 22:15:47 +0800

    adjustment (#659)
    * fix: tx success copy
    
    * fix: remove FeaturedProjects totally

--- src/pages/new-bridge/Send/TxSuccess.tsx
+++ src/pages/new-bridge/Send/TxSuccess.tsx
@@ -51,7 +51,7 @@ const TxSuccess = () => {
           </Tooltip>
         </Typography>
         <Link sx={{ color: "success.main" }} underline="always" href={transactionUrl} external>
-          Check your transaction in block explorer
+          Check your transaction in the block explorer
         </Link>
         <Typography sx={{ fontSize: "1.8rem", fontWeight: 700, mt: "4rem" }}>Deposit more {tokenSymbol}</Typography>
         <TextButton sx={{ color: "success.main" }} underline="always" onClick={handleReturnDeposit}>
@@ -76,7 +76,7 @@ const TxSuccess = () => {
           </Tooltip>
         </Typography>
         <Link sx={{ color: "success.main", marginBottom: "4.8rem" }} underline="always" href={transactionUrl} external>
-          Check your transaction in block explorer
+          Check your transaction in the block explorer
         </Link>
 
         <Box sx={{ flex: 1, display: "flex", alignItems: "flex-end", width: "100%", marginTop: "4.8rem", justifyContent: "center" }}>
@@ -100,7 +100,7 @@ const TxSuccess = () => {
         </Tooltip>
       </Typography>
       <Link sx={{ color: "success.main" }} href={transactionUrl} underline="always" external>
-        Check your transaction in block explorer
+        Check your transaction in the block explorer
       </Link>
       <Typography sx={{ fontSize: "1.8rem", fontWeight: 700, mt: "4rem" }}>Withdraw more {tokenSymbol}</Typography>
       <TextButton sx={{ color: "success.main" }} underline="always" onClick={handleReturnWithdraw}>
