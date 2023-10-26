6905e2d22e64c180bb0d8e8074d7feed490e0896
holybasil <holybasil.1128@gmail.com>
2023-10-26 21:31:40 +0800

    fix: route name

--- src/components/Header/useCheckTheme.tsx
+++ src/components/Header/useCheckTheme.tsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom"
 
 const useCheckTheme = () => {
   const { pathname } = useLocation()
-  const dark = useMemo(() => pathname === "/developer-nft/coming-soon", [pathname])
+  const dark = useMemo(() => pathname === "/developer-nft/check-eligibility", [pathname])
 
   return dark
 }
