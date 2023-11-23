4eefdebde386b214f06c9523d9aa6a1eb035e5f8
holybasil <holybasil.1128@gmail.com>
2023-11-23 10:28:51 +0800

    fix: mobile style && the final contract addr

--- src/pages/developer-nft/mint/home/MintFlowDialog.tsx
+++ src/pages/developer-nft/mint/home/MintFlowDialog.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react"
+import { makeStyles } from "tss-react/mui"
 
 import {
   AppBar, // Slide,useScrollTrigger
@@ -17,18 +18,27 @@ import { SCROLL_ORIGINS_NFT } from "@/constants"
 
 import MintFlow from "../flow"
 
+const useStyles = makeStyles()(theme => ({
+  paper: {
+    [theme.breakpoints.down("sm")]: {
+      margin: 0,
+    },
+  },
+}))
+
 const MintFlowDialog = props => {
   const scrollRef = useRef()
+  const { classes } = useStyles()
 
   // const trigger = useScrollTrigger({ target: scrollRef?.current })
   // console.log(trigger, "trigger")
   return (
-    <Dialog PaperProps={{ ref: scrollRef }} fullScreen open={props.open} {...props}>
+    <Dialog PaperProps={{ ref: scrollRef }} classes={{ paper: classes.paper }} fullScreen open={props.open} {...props}>
       {/* <Slide appear={false} direction="down" in={!trigger}> */}
       <AppBar sx={{ position: "sticky", top: 0, backgroundColor: theme => theme.palette.text.primary, paddingRight: "0 !important" }}>
         <Toolbar sx={{ justifyContent: "space-between", px: ["2rem", "2rem", "6rem"] }}>
           <ScrollLogo light></ScrollLogo>
-          <Typography sx={{ fontSize: "1.8rem", fontWeight: 600, color: theme => theme.palette.primary.contrastText }}>
+          <Typography sx={{ fontSize: ["1.6rem", "1.8rem"], fontWeight: 600, color: theme => theme.palette.primary.contrastText }}>
             Mint {SCROLL_ORIGINS_NFT}
           </Typography>
           <Stack direction="row" justifyContent="flex-end" sx={{ width: "8rem" }}>
