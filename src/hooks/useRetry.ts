71ba7637c95aaee92743f28133a027b2d5e976f6
zhengqi zhang <770166635@qq.com>
2024-01-25 10:56:26 +0800

    Revert "Revert "Bridge costs optimization (#916)""
    This reverts commit b80ffc8879aa62ffa5558ef75ea11bb8e2d41dd2.

--- src/hooks/useRetry.ts
+++ src/hooks/useRetry.ts
@@ -1,9 +1,8 @@
 import { ethers } from "ethers"
 import { useState } from "react"
 
-import L1MessageQueue from "@/assets/abis/L1MessageQueue.json"
 import L1ScrollMessenger from "@/assets/abis/L1ScrollMessenger.json"
-import L2GasPriceOracle from "@/assets/abis/L2GasPriceOracle.json"
+import L1MessageQueueWithGasPriceOracle from "@/assets/abis/L1_MESSAGE_QUEUE_WITH_GAS_PRICE_ORACLE.json"
 import { CHAIN_ID } from "@/constants"
 import { useBridgeContext } from "@/contexts/BridgeContextProvider"
 import useTxStore from "@/stores/txStore"
@@ -19,9 +18,12 @@ export function useRetry(props) {
   const replayMessage = async () => {
     const l2provider = networksAndSigners[CHAIN_ID.L2].provider
     const deployer = networksAndSigners[CHAIN_ID.L1].signer
-    const queue = new ethers.Contract(requireEnv("REACT_APP_L1_MESSAGE_QUEUE"), L1MessageQueue, deployer)
     const messenger = new ethers.Contract(requireEnv("REACT_APP_L1_SCROLL_MESSENGER"), L1ScrollMessenger, deployer)
-    const oracle = new ethers.Contract(requireEnv("REACT_APP_L2_GAS_PRICE_ORACLE"), L2GasPriceOracle, deployer)
+    const l1MessageQueueWithGasPriceOracleContract = new ethers.Contract(
+      requireEnv("REACT_APP_L1_MESSAGE_QUEUE_WITH_GAS_PRICE_ORACLE"),
+      L1MessageQueueWithGasPriceOracle,
+      deployer,
+    )
 
     setLoading(true)
     try {
@@ -31,7 +33,7 @@ export function useRetry(props) {
 
       for (const log of receipts.logs) {
         if (log.topics[0] === "0x69cfcb8e6d4192b8aba9902243912587f37e550d75c1fa801491fce26717f37e") {
-          const event = queue.interface.decodeEventLog("QueueTransaction", log.data, log.topics)
+          const event = l1MessageQueueWithGasPriceOracleContract.interface.decodeEventLog("QueueTransaction", log.data, log.topics)
           gasLimit = await l2provider.estimateGas({
             from: event.sender,
             to: event.target,
@@ -40,7 +42,7 @@ export function useRetry(props) {
           })
         } else if (log.topics[0] === "0x104371f3b442861a2a7b82a070afbbaab748bb13757bf47769e170e37809ec1e") {
           const event = messenger.interface.decodeEventLog("SentMessage", log.data, log.topics)
-          const gasPirce = await oracle.estimateCrossDomainMessageFee((gasLimit * BigInt(12)) / BigInt(10))
+          const gasPirce = await l1MessageQueueWithGasPriceOracleContract.estimateCrossDomainMessageFee((gasLimit * BigInt(12)) / BigInt(10))
 
           await messenger.replayMessage(
             event.sender,
