65125b6c365b1b3cad9eca6994d63e84173e0a2f
holybasil <holybasil.1128@gmail.com>
2024-02-28 13:56:49 +0800

    fix: coupon share

--- src/components/Header/useCheckTheme.tsx
+++ src/components/Header/useCheckTheme.tsx
@@ -1,19 +1,20 @@
 import { useMemo } from "react"
-import { useLocation } from "react-router-dom"
+import { useLocation, useMatch } from "react-router-dom"
 
 const useCheckTheme = () => {
   const { pathname } = useLocation()
-  // const isInvite = useMatch("/invite/:code")
+  const isInvite = useMatch("/invite/:code")
   const dark = useMemo(() => {
-    return [
-      "/developer-nft/check-eligibility",
-      "/developer-nft/mint",
-      "/scroll-skelly",
-      "/scroll-skelly/mint",
-      "/scroll-skelly/dashboard",
-      "/scroll-skelly/badge",
-    ].includes(pathname)
-    // || isInvite
+    return (
+      [
+        "/developer-nft/check-eligibility",
+        "/developer-nft/mint",
+        "/scroll-skelly",
+        "/scroll-skelly/mint",
+        "/scroll-skelly/dashboard",
+        "/scroll-skelly/badge",
+      ].includes(pathname) || isInvite
+    )
   }, [pathname])
 
   return dark
