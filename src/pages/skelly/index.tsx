fa70683be6d2ca11c5738a6ae04d92df4697e765
holybasil <holybasil.1128@gmail.com>
2024-03-12 11:12:36 +0800

    fix: view my skelly

--- src/pages/skelly/index.tsx
+++ src/pages/skelly/index.tsx
@@ -11,7 +11,6 @@ import LoadingPage from "./loading"
 import WrongNetwork from "./wrongNetwork"
 
 const SkellyIndex = props => {
-  const { code } = props
   const navigate = useNavigate()
   const isOthersSkelly = useMatch("/scroll-skelly/:address")
   const isBadgeDetail = useMatch("/scroll-skelly/badge/:id")
@@ -21,7 +20,7 @@ const SkellyIndex = props => {
   const { profileMintedLoading, profileMinted, checkIfProfileMinted, changeMintStep } = useSkellyStore()
   console.log(profileMinted, "profileMinted skelly homepage")
   const isWrongNetwork = useMemo(() => {
-    return !isOthersSkelly && !isBadgeDetail && chainId && chainId !== CHAIN_ID.L2
+    return !isOthersSkelly && !isBadgeDetail && chainId !== CHAIN_ID.L2
   }, [chainId, isOthersSkelly, isBadgeDetail])
 
   useEffect(() => {
@@ -33,27 +32,21 @@ const SkellyIndex = props => {
     }
   }, [isWrongNetwork, unsignedProfileRegistryContract, walletCurrentAddress])
 
-  // cons
+  const renderSkelly = () => {
+    if (!walletCurrentAddress) {
+      return <Navigate to="/scroll-skelly/mint" replace={true}></Navigate>
+    } else if (isWrongNetwork) {
+      return <WrongNetwork></WrongNetwork>
+    } else if (profileMintedLoading) {
+      return <LoadingPage></LoadingPage>
+    } else if (profileMinted) {
+      // if connected user views the invite link, then redirect to skelly
+      return <Dashboard />
+    }
+    return <Navigate to="/scroll-skelly/mint" replace={true}></Navigate>
+  }
 
-  // if connected user views the invite link, then redirect to skelly
-  return (
-    <>
-      {isWrongNetwork ? (
-        <WrongNetwork></WrongNetwork>
-      ) : (
-        <>
-          {profileMintedLoading ? (
-            <LoadingPage></LoadingPage>
-          ) : (
-            <>
-              {profileMinted === true && <>{code ? <Navigate to="/scroll-skelly" replace={true}></Navigate> : <Dashboard />}</>}
-              {profileMinted === false && <Navigate to="/scroll-skelly/mint" replace={true}></Navigate>}
-            </>
-          )}
-        </>
-      )}
-    </>
-  )
+  return <>{renderSkelly()}</>
 }
 
 export default SkellyIndex
