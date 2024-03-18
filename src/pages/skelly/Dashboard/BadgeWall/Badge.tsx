362fecd41388e4b5a1b351be5951b4fb64f46d7a
holybasil <holybasil.1128@gmail.com>
2024-03-18 10:51:25 +0800

    fix: heartbeat tooltip

--- src/pages/skelly/Dashboard/BadgeWall/Badge.tsx
+++ src/pages/skelly/Dashboard/BadgeWall/Badge.tsx
@@ -1,20 +1,17 @@
 import Img from "react-cool-img"
 
-import { Box, Tooltip } from "@mui/material"
-import { tooltipClasses } from "@mui/material/Tooltip"
+import { Box } from "@mui/material"
 import { styled } from "@mui/system"
 
 import useSkellyStore, { BadgeDetailDialogTpye } from "@/stores/skellyStore"
 import { getBadgeImgURL } from "@/utils"
 
-import NameTip from "../../components/NameTip"
+import ToolTip from "../../components/Tooltip"
 
 const BadgeBox = styled(Box)(({ theme }) => ({
   cursor: "pointer",
   position: "absolute",
 }))
-
-const CustomTooltip = styled(Tooltip)(({ theme }) => ({}))
 
 const Badge = ({ badge, index, badgewidth }) => {
   const { changeBadgeDetailDialog, changeSelectedBadge } = useSkellyStore()
@@ -25,50 +22,7 @@ const Badge = ({ badge, index, badgewidth }) => {
   }
 
   return (
-    <CustomTooltip
-      title={<NameTip metadata={badge.metadata}></NameTip>}
-      followCursor
-      PopperProps={{
-        popperOptions: {
-          modifiers: [
-            {
-              name: "offset",
-              options: {
-                offset: ({ placement, reference, popper }) => {
-                  if (placement === "bottom") {
-                    return [popper.width / 4, 27]
-                  } else {
-                    return [popper.width / 4, 12]
-                  }
-                },
-              },
-            },
-          ],
-        },
-      }}
-      slotProps={{
-        popper: {
-          sx: {
-            [`&.${tooltipClasses.popper}[data-popper-placement*="bottom"] .${tooltipClasses.tooltip}`]: {
-              marginTop: "-6px",
-              background: "transparent",
-            },
-            [`&.${tooltipClasses.popper}[data-popper-placement*="top"] .${tooltipClasses.tooltip}`]: {
-              marginBottom: "-6px",
-              background: "transparent",
-            },
-            [`&.${tooltipClasses.popper}[data-popper-placement*="right"] .${tooltipClasses.tooltip}`]: {
-              marginLeft: "-6px",
-              background: "transparent",
-            },
-            [`&.${tooltipClasses.popper}[data-popper-placement*="left"] .${tooltipClasses.tooltip}`]: {
-              marginRight: "-6px",
-              background: "transparent",
-            },
-          },
-        },
-      }}
-    >
+    <ToolTip title={<Box sx={{ fontWeight: 600 }}>{badge.metadata?.name}</Box>}>
       <BadgeBox
         key={index}
         style={{
@@ -87,7 +41,7 @@ const Badge = ({ badge, index, badgewidth }) => {
           placeholder="/imgs/skelly/badgePlaceholder.svg"
         />
       </BadgeBox>
-    </CustomTooltip>
+    </ToolTip>
   )
 }
 
--- src/pages/skelly/invite/Invite.tsx
+++ src/pages/skelly/invite/Invite.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom"
 
+import { useRainbowContext } from "@/contexts/RainbowProvider"
 import useSkellyStore from "@/stores/skellyStore"
 
 import LoadingPage from "../loading"
@@ -8,9 +9,11 @@ import ReferralCode from "../mint/ReferralCode"
 const SkellyInvite = props => {
   const { code } = props
 
+  const { walletCurrentAddress } = useRainbowContext()
+
   const { profileMintedLoading, profileMinted } = useSkellyStore()
 
-  if (profileMintedLoading || profileMinted === null) {
+  if ((profileMintedLoading || profileMinted === null) && walletCurrentAddress) {
     return <LoadingPage></LoadingPage>
   }
   if (profileMinted) {
