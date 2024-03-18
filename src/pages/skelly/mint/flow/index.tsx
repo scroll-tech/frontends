0277eff2fe7a9f78b99701a2182abcb8afc2ab72
holybasil <holybasil.1128@gmail.com>
2024-03-18 17:17:00 +0800

    fix: onboarding

---
+++ src/pages/skelly/mint/flow/index.tsx
@@ -0,0 +1,38 @@
+import "swiper/css"
+import "swiper/css/pagination"
+import { Pagination } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+import SectionWrapper from "@/components/SectionWrapper"
+
+import FirstBadgeStep from "./FirstBadgeStep"
+import MintStep from "./MintStep"
+import NameStep from "./NameStep"
+
+const MintFlow = props => {
+  const { scrollTarget } = props
+
+  return (
+    <SectionWrapper dark full sx={{ p: "0 !important", "& .MuiTypography-root": { color: theme => theme.palette.primary.contrastText } }}>
+      <Swiper
+        style={{ zIndex: 0 }}
+        pagination={{
+          type: "progressbar",
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <NameStep scrollTarget={scrollTarget}></NameStep>
+        </SwiperSlide>
+        <SwiperSlide>
+          <MintStep scrollTarget={scrollTarget}></MintStep>
+        </SwiperSlide>
+        <SwiperSlide>
+          <FirstBadgeStep scrollTarget={scrollTarget}></FirstBadgeStep>
+        </SwiperSlide>
+      </Swiper>
+    </SectionWrapper>
+  )
+}
+
+export default MintFlow
