ba7cd9c7c6367c370d147634ab84afe3f124d3e9
zhengqi zhang <770166635@qq.com>
2024-03-25 18:33:34 +0800

    Update Earn badges for Scroll Skelly styles

--- src/pages/ecosystem/Canvas/BadgeList/index.tsx
+++ src/pages/ecosystem/Canvas/BadgeList/index.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react"
+import React, { useMemo, useState } from "react"
 
-import { Box, Card, CardContent, Stack, SvgIcon, Typography } from "@mui/material"
+import { Box, Card, CardContent, IconButton, Stack, SvgIcon, Typography } from "@mui/material"
 import { styled } from "@mui/system"
 
-import { ReactComponent as LogoSvg } from "@/assets/svgs/common/scroll-logo-icon.svg"
-
-// import { ReactComponent as LeftSvg } from "@/assets/svgs/canvas/arrow-left.svg"
-// import { ReactComponent as RightSvg } from "@/assets/svgs/canvas/arrow-right.svg"
+import { ReactComponent as LeftSvg } from "@/assets/svgs/canvas/arrow-left.svg"
+import { ReactComponent as RightSvg } from "@/assets/svgs/canvas/arrow-right.svg"
 
 interface CarouselItem {
-  title: string
+  name: string
+  nftAddress: string[] | null
+  nftAbi: object | null
+  badgeContract: string
   description: string
+  image: string
+  native: boolean
+  originsNFT?: boolean
+  issuer: {
+    origin: string
+    name: string
+    logo: string
+  }
+  validator: (address, provider) => Promise<boolean> | boolean
 }
 
 interface CarouselProps {
@@ -18,45 +28,59 @@ interface CarouselProps {
 }
 
 const BadgeCard = styled(Card)(({ theme }) => ({
-  width: "44.4rem",
+  // minWidth: "44.4rem",
+  width: "calc((100% - 3.2rem) / 3)",
   height: "44.4rem",
   padding: "1.6rem",
   borderRadius: "2rem",
   background: "#FFF8F3",
   textAlign: "center",
-  transition: "transform 0.5s ease", // Add transition for smooth scroll
+  transition: "transform 0.5s ease",
+  flex: " 0 0 auto",
+  cursor: "pointer",
+  "&  *": {
+    cursor: "pointer !important",
+  },
 }))
 
-// const ArrorButton = styled(IconButton)(({ theme }) => ({
-//   background: "#FFF8F3",
-//   width: "4.8rem",
-//   height: "4.8rem",
-//   borderRadius: "0.8rem",
-//   display: "flex",
-//   justifyContent: "center",
-//   alignItems: "center",
-//   "&:hover": {
-//     background: "#FFF8F3",
-//   },
-//   "&.Mui-disabled": {
-//     color: "#ccc",
-//     background: "#F0F0F0",
-//   },
-// }))
+const ArrorButton = styled(IconButton)(({ theme }) => ({
+  background: "#FFF8F3",
+  width: "4.8rem",
+  height: "4.8rem",
+  borderRadius: "0.8rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  "&:hover": {
+    background: "#FFF8F3",
+  },
+  "&.Mui-disabled": {
+    color: "#ccc",
+    background: "#F0F0F0",
+  },
+}))
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
-  const [currentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-  // const handlePrev = () => {
-  //   setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : items.length - 3))
-  // }
+  const handlePrev = () => {
+    setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : items.length - 3))
+  }
 
-  // const handleNext = () => {
-  //   setCurrentIndex(prevIndex => (prevIndex < items.length - 3 ? prevIndex + 1 : 0))
-  // }
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex < items.length - 3 ? prevIndex + 1 : 0))
+  }
 
-  // const isPrevDisabled = currentIndex === 0
-  // const isNextDisabled = currentIndex >= items.length - 3
+  const isPrevDisabled = currentIndex === 0
+  const isNextDisabled = (currentIndex + 1) * 3 >= items.length
+
+  const translateX = useMemo(() => {
+    return `translateX(calc(((100% + 1.6rem)  * -1) * ${currentIndex}))`
+  }, [currentIndex])
+
+  const handleCardClick = item => {
+    window.open(item.issuer.origin, "_blank")
+  }
 
   return (
     <Box sx={{ overflow: "hidden" }}>
@@ -65,33 +89,34 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
         justifyContent="space-between"
         spacing={2}
         sx={{ margin: "5rem 0" }}
-        style={{ transform: `translateX(-${currentIndex * (100 / 3)}%)`, transition: "transform 0.5s ease" }}
+        style={{ transform: translateX, transition: "transform 0.5s ease" }}
       >
         {items.map((item, index) => (
-          <BadgeCard key={index} sx={{ minWidth: 200 }}>
+          <BadgeCard onClick={() => handleCardClick(item)} key={index}>
             <CardContent>
-              <SvgIcon component={LogoSvg} sx={{ fontSize: "8.8rem" }} inheritViewBox />
+              <img alt="logo" src={item.image} style={{ height: "8.8rem", borderRadius: "0.8rem" }} />
               <Typography variant="h5" sx={{ margin: "1.6rem 0" }} component="div">
-                {item.title}
+                {item.name}
               </Typography>
-              <Typography variant="h6" sx={{ fontWeight: 400, lineHeight: "3.2rem", marginBottom: "4.4rem" }}>
+              <Typography variant="h6" sx={{ fontWeight: 400, lineHeight: "3.2rem", marginBottom: "4.4rem", maxHeight: "14rem", overflow: "auto" }}>
                 {item.description}
               </Typography>
               <Typography variant="h5">
-                <SvgIcon component={LogoSvg} sx={{ fontSize: "4.8rem" }} inheritViewBox /> Dawn Wallet
+                <img alt="issuer logo" src={item.issuer.logo} style={{ height: "4.8rem", borderRadius: "0.8rem" }} />
+                {item.issuer.name}
               </Typography>
             </CardContent>
           </BadgeCard>
         ))}
       </Stack>
-      {/* <Box display="flex" justifyContent="flex-end" gap={"1.6rem"} marginTop={2} paddingBottom="9.5rem">
+      <Box display="flex" justifyContent="flex-end" gap={"1.6rem"} marginTop={2} paddingBottom="9.5rem">
         <ArrorButton onClick={handlePrev} disabled={isPrevDisabled} aria-label="previous">
           <SvgIcon component={LeftSvg} inheritViewBox />
         </ArrorButton>
         <ArrorButton onClick={handleNext} disabled={isNextDisabled} aria-label="next">
           <SvgIcon component={RightSvg} sx={{ fontSize: "2.2rem" }} inheritViewBox />
         </ArrorButton>
-      </Box> */}
+      </Box>
     </Box>
   )
 }
