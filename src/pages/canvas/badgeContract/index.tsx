b8bd7dc51649ad4cd82b31af8cd758316379e1e0
holybasil <holybasil.1128@gmail.com>
2024-03-27 15:06:33 +0800

    refactor: badge detail/contract

--- src/pages/canvas/badgeContract/index.tsx
+++ src/pages/canvas/badgeContract/index.tsx
@@ -1,43 +1,26 @@
 import { useMemo, useState } from "react"
-import Img from "react-cool-img"
-import { Helmet } from "react-helmet-async"
 import { useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 
-import { Avatar, Box, Stack, SvgIcon, Typography } from "@mui/material"
-import { styled } from "@mui/material/styles"
+import { Stack, SvgIcon, Typography } from "@mui/material"
 
 import { ReactComponent as ValidSvg } from "@/assets/svgs/canvas/check.svg"
 import { ReactComponent as WarningSvg } from "@/assets/svgs/canvas/circle-warning.svg"
 import { ReactComponent as ShareSvg } from "@/assets/svgs/canvas/share.svg"
 import ScrollButton from "@/components/Button"
 import Link from "@/components/Link"
-import SectionWrapper from "@/components/SectionWrapper"
-import { ANNOUNCING_SCROLL_ORIGINS_NFT, DESIGNING_SCROLL_ORIGINS } from "@/constants"
 import { useRainbowContext } from "@/contexts/RainbowProvider"
 import { useAsyncMemo } from "@/hooks"
-import useCheckViewport from "@/hooks/useCheckViewport"
-import { mintBadge, queryUserBadges } from "@/services/canvasService"
+import { checkIfHasBadgeByAddress, mintBadge } from "@/services/canvasService"
 import useCanvasStore from "@/stores/canvasStore"
-import { decodeBadgePayload, generateShareTwitterURL, getBadgeImgURL, requireEnv } from "@/utils"
+import { generateShareTwitterURL, requireEnv } from "@/utils"
 
 import Badges, { badgeMap } from "../Dashboard/UpgradeDialog/Badges"
-import Statistic from "../components/Statistic"
+import BadgeDetail from "../badge/BadgeDetail"
 
-const isOriginsNFTBadge = badgeContract => {
-  return badgeMap[badgeContract]?.originsNFT
-}
 const isNativeBadge = badgeContract => {
   return badgeMap[badgeContract]?.native
 }
-
-const CustomLink = styled(Link)(({ theme }) => ({
-  color: `${theme.palette.primary.main} !important`,
-  fontSize: "inherit",
-  textUnderlineOffset: "2px",
-  textDecorationThickness: "1px",
-  fontWeight: 700,
-}))
 
 const BadgeContractDetail = props => {
   const { address } = useParams()
@@ -51,14 +34,12 @@ const BadgeContractDetail = props => {
     return badgeMap[address]
   }, [address])
 
-  // TODO: pre fetch?
   const isOwned = useAsyncMemo(async () => {
-    const userBadges = await queryUserBadges(walletCurrentAddress)
-
-    const userBadgeContracts = userBadges.map(item => decodeBadgePayload(item.data)[0])
-
-    return userBadgeContracts.includes(address)
-  }, [address, walletCurrentAddress])
+    if (provider && detail.badgeContract) {
+      const hasBadge = await checkIfHasBadgeByAddress(provider, walletCurrentAddress, detail.badgeContract)
+      return hasBadge
+    }
+  }, [provider, walletCurrentAddress, detail.badgeContract])
 
   const isValid = useAsyncMemo(async () => {
     const badgeForMint = Badges.find(item => item.badgeContract === address)
@@ -73,15 +54,13 @@ const BadgeContractDetail = props => {
 
   const metadata = useMemo(
     () => ({
-      title: `I found a badge called ${detail.name} you may like`,
-      description: detail.description,
+      title: `Canvas Badge - ${detail.name}`,
+      description: `I found a badge called ${detail.name} you may like`,
       // TODO:
       image: "",
     }),
     [detail],
   )
-
-  const { isPortrait, isLandscape } = useCheckViewport()
 
   const handleMint = async () => {
     setLoading(true)
@@ -130,7 +109,6 @@ const BadgeContractDetail = props => {
     return null
   }
 
-  // TODO: async need loading?
   const renderAction = () => {
     if (!walletCurrentAddress) {
       return (
@@ -150,118 +128,37 @@ const BadgeContractDetail = props => {
           Visit Scroll Canvas
         </ScrollButton>
       )
+    } else if (profileMinted === true && !isOwned) {
+      return (
+        <ScrollButton color="primary" onClick={handleMint} loading={loading} gloomy={!isValid}>
+          Mint now
+        </ScrollButton>
+      )
     }
-    return (
-      <ScrollButton color="primary" onClick={handleMint} loading={loading} gloomy={!isValid}>
-        Mint now
-      </ScrollButton>
-    )
+    return null
   }
 
   return (
-    <>
-      <Helmet>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Helmet>
-      <SectionWrapper
-        dark
-        sx={{
-          pt: ["2.4rem", "4rem", "8rem"],
-          pb: ["8rem", "16rem"],
-          minHeight: "calc(100vh - 6.5rem)",
-          // display: "flex",
-          // justifyContent: "center",
-          // alignItems: "center",
-        }}
-      >
-        <Box
-          sx={{
-            height: "100%",
-            display: "grid",
-            gridTemplateColumns: "min-content max-content",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#101010",
-            columnGap: "4rem",
-            rowGap: "7.2rem",
-            "& .MuiTypography-root": {
-              color: theme => theme.palette.primary.contrastText,
-            },
-            "@media (max-width: 1280px)": {
-              gap: "4rem",
-              // gridTemplateColumns: "minmax(min-content, 1fr) 1fr",
-              gridTemplateColumns: "min-content 1fr",
-              justifyItems: "center",
-            },
-
-            "@media (max-width: 900px)": {
-              gap: "2rem",
-              gridTemplateColumns: "1fr",
-            },
-            "@media (max-width: 600px)": {
-              gap: "2.4rem",
-            },
-          }}
-        >
-          <Box sx={{ width: "48rem", aspectRatio: "1/1" }}>
-            <Img src={getBadgeImgURL(detail.image)} style={{ borderRadius: "0.8rem" }} alt="badge image" />
-          </Box>
-          <Stack direction="column" spacing={isPortrait ? "2.4rem" : "3.6rem"} alignItems={isLandscape ? "flex-start" : "center"}>
-            <Box sx={{ textAlign: ["center", "center", "left"] }}>
-              <Typography sx={{ fontSize: ["4rem", "5.6rem"], fontWeight: 600, lineHeight: ["5.6rem", "9.6rem"] }}>{detail.name}</Typography>
-
-              <Typography sx={{ fontSize: ["1.6rem", "2rem"], lineHeight: ["2.4rem", "3.2rem"], maxWidth: ["100%", "56rem"] }}>
-                {isOriginsNFTBadge(detail.badgeContract) ? (
-                  <>
-                    <CustomLink href={ANNOUNCING_SCROLL_ORIGINS_NFT} underline="always" external>
-                      Scroll Origins
-                    </CustomLink>{" "}
-                    is a{" "}
-                    <CustomLink href={DESIGNING_SCROLL_ORIGINS} underline="always" external>
-                      specially designed NFT
-                    </CustomLink>{" "}
-                    program to celebrate alongside early developers building on Scroll within 60 days of Genesis Block (Before December 9, 2023
-                    10:59PM GMT).
-                  </>
-                ) : (
-                  <>{detail.description}</>
-                )}
-              </Typography>
-            </Box>
-
-            <Box>
-              <Statistic label="Issued by">
-                <Avatar src={detail.issuer?.logo}></Avatar>
-                {detail.issuer?.name}
-              </Statistic>
-            </Box>
-
-            <Stack direction="row" gap="1.6rem" alignItems="center">
-              {renderAction()}
-              {!isNativeBadge && (
-                <ScrollButton color="secondary" href={detail.issuer?.origin} target="_blank">
-                  Visit {detail.issuer?.name}
-                </ScrollButton>
-              )}
-
-              <Link external href={shareBadgeURL}>
-                <SvgIcon sx={{ fontSize: "3.2rem", color: "primary.contrastText" }} component={ShareSvg} inheritViewBox></SvgIcon>
-              </Link>
-            </Stack>
-            <Stack direction="row" gap="0.8rem" alignItems="center">
-              {renderTip()}
-            </Stack>
-          </Stack>
-        </Box>
-      </SectionWrapper>
-    </>
+    <BadgeDetail
+      detail={detail}
+      metadata={metadata}
+      property={["issuer"]}
+      extra={
+        <Stack direction="row" gap="0.8rem" alignItems="center">
+          {renderTip()}
+        </Stack>
+      }
+    >
+      {renderAction()}
+      {!isNativeBadge && (
+        <ScrollButton color="secondary" href={detail.issuer?.origin} target="_blank">
+          Visit {detail.issuer?.name}
+        </ScrollButton>
+      )}
+      <Link external href={shareBadgeURL}>
+        <SvgIcon sx={{ fontSize: "3.2rem", color: "primary.contrastText" }} component={ShareSvg} inheritViewBox></SvgIcon>
+      </Link>
+    </BadgeDetail>
   )
 }
 
