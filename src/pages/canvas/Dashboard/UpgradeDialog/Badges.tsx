18b68d9bb67999d4734cee1a99b12cf4738bc5cd
holybasil <holybasil.1128@gmail.com>
2024-03-27 15:06:33 +0800

    refactor: badge detail/contract

--- src/pages/canvas/Dashboard/UpgradeDialog/Badges.tsx
+++ src/pages/canvas/Dashboard/UpgradeDialog/Badges.tsx
@@ -1,6 +1,10 @@
 import { ethers } from "ethers"
 
+import { styled } from "@mui/material/styles"
+
 import ScrollOriginsNFTABI from "@/assets/abis/ScrollOriginsNFT.json"
+import Link from "@/components/Link"
+import { ANNOUNCING_SCROLL_ORIGINS_NFT, DESIGNING_SCROLL_ORIGINS } from "@/constants"
 import { requireEnv } from "@/utils"
 
 const SCROLL_ORIGINS_NFT = requireEnv("REACT_APP_SCROLL_ORIGINS_NFT")
@@ -11,14 +15,35 @@ const SCROLL_SEPOLIA_SIMPLE_BADGE_A_ADDRESS = "0x54E0C87672ebEC2A4d86dF3BDbB5286
 const SCROLL_SEPOLIA_SIMPLE_BADGE_B_ADDRESS = "0xF03214B490B6d05527cAD0B99a2820356b97840B"
 const SCROLL_SEPOLIA_SIMPLE_BADGE_C_ADDRESS = "0x5892067fEB828020FBA7B3dD87428010Ecaa86a7"
 
+const CustomLink = styled(Link)(({ theme }) => ({
+  color: `${theme.palette.primary.main} !important`,
+  fontSize: "inherit",
+  textUnderlineOffset: "2px",
+  textDecorationThickness: "1px",
+  fontWeight: 700,
+}))
+
 const Badges = [
   {
     name: "Scroll Origins NFT",
     nftAddress: [SCROLL_ORIGINS_NFT, SCROLL_ORIGINS_NFT_V2],
     nftAbi: ScrollOriginsNFTABI,
     badgeContract: SCROLL_SEPOLIA_ORIGINS_BADGE_ADDRESS,
-    description:
+    metaDescription:
       "The Scroll Origins NFT is a soulbound NFT that serves to recognize the early builders of Scroll. Eligible minters have deployed a smart contract to Scroll Mainnetwithin 60 days of the Genesis Block. Higher levels of rarity are are rewarded to smart contracts that have contributed significant levels of interaction or value to the network.",
+    description: (
+      <>
+        <CustomLink href={ANNOUNCING_SCROLL_ORIGINS_NFT} underline="always" external>
+          Scroll Origins
+        </CustomLink>{" "}
+        is a{" "}
+        <CustomLink href={DESIGNING_SCROLL_ORIGINS} underline="always" external>
+          specially designed NFT
+        </CustomLink>{" "}
+        program to celebrate alongside early developers building on Scroll within 60 days of Genesis Block (Before December 9, 2023 10:59PM GMT).
+      </>
+    ),
+
     image: "/imgs/canvas/OriginsNFT.svg",
     native: true,
     originsNFT: true,
@@ -87,9 +112,9 @@ const Badges = [
 ]
 
 export const badgeMap = Object.fromEntries(
-  Badges.map(({ badgeContract, native, originsNFT, issuer, image, name, description }) => [
+  Badges.map(({ badgeContract, native, originsNFT, issuer, image, name, description, metaDescription }) => [
     badgeContract,
-    { native, originsNFT, issuer, image, badgeContract, name, description },
+    { native, originsNFT, issuer, image, badgeContract, name, description, metaDescription },
   ]),
 )
 
