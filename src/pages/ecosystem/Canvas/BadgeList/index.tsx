656a1786af6e3d02279fc12153a1b9230de03a4e
holybasil <holybasil.1128@gmail.com>
2024-03-28 16:41:02 +0800

    fix: ecosystem badge list

--- src/pages/ecosystem/Canvas/BadgeList/index.tsx
+++ src/pages/ecosystem/Canvas/BadgeList/index.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 import { Box, Card, CardContent, IconButton, Stack, SvgIcon, Typography } from "@mui/material"
 import { styled } from "@mui/system"
@@ -30,9 +31,8 @@ interface CarouselProps {
 
 const BadgeCard = styled(Card)(({ theme }) => ({
   // minWidth: "44.4rem",
-  width: "calc((100% - 3.2rem) / 3)",
+  width: "calc((100% - 6rem) / 3)",
   height: "44.4rem",
-  padding: "1.6rem",
   borderRadius: "2rem",
   background: "#FFF8F3",
   textAlign: "center",
@@ -63,6 +63,7 @@ const ArrorButton = styled(IconButton)(({ theme }) => ({
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const navigate = useNavigate()
 
   const handlePrev = () => {
     setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : items.length - 3))
@@ -76,11 +77,14 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
   const isNextDisabled = (currentIndex + 1) * 3 >= items.length
 
   const translateX = useMemo(() => {
-    return `translateX(calc(((100% + 1.6rem)  * -1) * ${currentIndex}))`
+    return `translateX(calc(((100% + 3rem)  * -1) * ${currentIndex}))`
   }, [currentIndex])
 
-  const handleCardClick = item => {
-    window.open(item.issuer.origin, "_blank")
+  const handleCardClick = (e, item) => {
+    // if a link in badge description is clicked, then do not open detail page
+    if (e.target.tagName !== "A") {
+      navigate(`/scroll-canvas/badge-contract/${item.badgeContract}`)
+    }
   }
 
   return (
@@ -88,22 +92,45 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
       <Stack
         direction="row"
         justifyContent="space-between"
-        spacing={2}
+        spacing="3rem"
         sx={{ margin: "5rem 0" }}
         style={{ transform: translateX, transition: "transform 0.5s ease" }}
       >
         {items.map((item, index) => (
-          <BadgeCard onClick={() => handleCardClick(item)} key={index}>
-            <CardContent>
+          <BadgeCard onClick={e => handleCardClick(e, item)} key={index}>
+            <CardContent
+              sx={{
+                padding: "3rem 3rem 4rem !important",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "2.4rem",
+              }}
+            >
               <img alt="logo" src={item.image} style={{ height: "8.8rem", borderRadius: "0.8rem" }} />
-              <Typography variant="h5" sx={{ margin: "1.6rem 0" }} component="div">
+              <Typography variant="h5" component="div">
                 {item.name}
               </Typography>
-              <Typography variant="h6" sx={{ fontWeight: 400, lineHeight: "3.2rem", marginBottom: "4.4rem", maxHeight: "14rem", overflow: "auto" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: 400,
+                  lineHeight: "3.2rem",
+                  mb: "1.2rem",
+                  flex: 1,
+
+                  display: "-webkit-box",
+                  width: "100%",
+                  "-webkit-box-orient": "vertical",
+                  "-webkit-line-clamp": "4",
+                  overflow: "hidden",
+                }}
+              >
                 {item.description}
               </Typography>
               <Typography variant="h5">
-                <img alt="issuer logo" src={item.issuer.logo} style={{ height: "4.8rem", borderRadius: "0.8rem" }} />
+                <img alt="issuer logo" src={item.issuer.logo} style={{ height: "4.8rem", borderRadius: "0.5rem", marginRight: "1.4rem" }} />
                 {item.issuer.name}
               </Typography>
             </CardContent>
