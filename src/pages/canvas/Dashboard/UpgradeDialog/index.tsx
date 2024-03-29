9ff414d6b6c40f9b4daf2645cc2e6e1473848011
holybasil <holybasil.1128@gmail.com>
2024-03-29 10:10:40 +0800

    refactor: clean up dialog

--- src/pages/canvas/Dashboard/UpgradeDialog/index.tsx
+++ src/pages/canvas/Dashboard/UpgradeDialog/index.tsx
@@ -2,52 +2,26 @@ import { useEffect, useState } from "react"
 
 // import Img from "react-cool-img"
 // import { useNavigate } from "react-router-dom"
-import { Dialog, DialogContent, DialogTitle, IconButton, List, SvgIcon, Typography } from "@mui/material"
+import { List } from "@mui/material"
 import { styled } from "@mui/system"
 
-// import { ReactComponent as BackSvg } from "@/assets/svgs/canvas/back.svg"
-import { ReactComponent as CloseSvg } from "@/assets/svgs/canvas/close.svg"
-// import Button from "@/components/Button"
 import { useRainbowContext } from "@/contexts/RainbowProvider"
 import useCanvasStore from "@/stores/canvasStore"
 
+import Dialog from "../../components/Dialog"
 import Empty from "../../components/Empty"
 import BadgeItem from "./BadgeItem"
 import Badges from "./Badges"
 
-const StyledDialog = styled(Dialog)(({ theme }) => ({
-  backgroundColor: "rgba(16, 16, 16, 0.60)",
-  "& .MuiDialog-paper": {
-    // background: "linear-gradient(114deg, #2A2A2A 0%, rgba(27, 27, 27, 0.60) 100%)",
-    backgroundColor: "#101010",
-    width: "64rem",
-    height: "67.4rem",
-    padding: "3.2rem",
-    borderRadius: "1.6rem",
-  },
-}))
-
-const StyledDialogTitle = styled(DialogTitle)(({ theme }) => ({
-  padding: 0,
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  marginBottom: "2.4rem",
-  marginRight: "-0.8rem",
-}))
-
-const StyledDialogContent = styled(DialogContent)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "0",
-}))
-
 const StyledList = styled(List)(({ theme }) => ({
-  width: "100%",
+  width: "57.6rem",
+  height: "62.4rem",
   display: "flex",
   flexDirection: "column",
   gap: "2.4rem",
+  padding: "2.4rem 0 0",
+  overflowY: "auto",
+
   "& .MuiListItem-root": {
     gap: "1.6rem",
   },
@@ -90,43 +64,20 @@ const UpgradeDialog = () => {
 
   if (!visibleBadges.length) {
     return (
-      <StyledDialog maxWidth={false} onClose={handleClose} open={upgradeDialogVisible}>
-        <StyledDialogTitle
-          sx={{
-            // m: 0,
-            // p: ["2rem", "3rem"],
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-          }}
-        >
-          <IconButton sx={{ "&:hover": { backgroundColor: "unset" } }} onClick={handleClose}>
-            <SvgIcon sx={{ fontSize: ["1.6rem", "1.8rem"], color: "#fff" }} component={CloseSvg} inheritViewBox></SvgIcon>
-          </IconButton>
-        </StyledDialogTitle>
-        <DialogContent>
-          <Empty title="No eligible badges for minting"></Empty>
-        </DialogContent>
-      </StyledDialog>
+      <Dialog onClose={handleClose} open={upgradeDialogVisible}>
+        <Empty title="No eligible badges for minting" sx={{ width: "57.6rem", height: "62.7rem" }}></Empty>
+      </Dialog>
     )
   }
 
   return (
-    <StyledDialog maxWidth={false} onClose={handleClose} open={upgradeDialogVisible}>
-      <StyledDialogTitle>
-        <Typography sx={{ fontSize: "3.2rem", lineHeight: "5.6rem", color: "#ffffff", fontWeight: 600 }}>Badges for mint</Typography>
-        <IconButton sx={{ "&:hover": { backgroundColor: "unset" } }} onClick={handleClose}>
-          <SvgIcon sx={{ fontSize: ["1.6rem", "2.4rem"], color: "#fff" }} component={CloseSvg} inheritViewBox></SvgIcon>
-        </IconButton>
-      </StyledDialogTitle>
-      <StyledDialogContent>
-        <StyledList>
-          {visibleBadges.map((badge, index) => (
-            <BadgeItem key={index} badge={badge} />
-          ))}
-        </StyledList>
-      </StyledDialogContent>
-    </StyledDialog>
+    <Dialog title="Badges for mint" open={upgradeDialogVisible} onClose={handleClose}>
+      <StyledList>
+        {visibleBadges.map((badge, index) => (
+          <BadgeItem key={index} badge={badge} />
+        ))}
+      </StyledList>
+    </Dialog>
   )
 }
 
---
+++ src/pages/canvas/components/Dialog/index.tsx
@@ -0,0 +1,51 @@
+import { Dialog, DialogContent, DialogTitle, IconButton, SvgIcon, Typography } from "@mui/material"
+
+import { ReactComponent as BackSvg } from "@/assets/svgs/canvas/back.svg"
+import { ReactComponent as CloseSvg } from "@/assets/svgs/canvas/close.svg"
+
+const ScrollDialog = props => {
+  const { title, fullWidth, allowBack, onClose, onBack, children, extra, ...restProps } = props
+  return (
+    <Dialog
+      maxWidth={false}
+      sx={{
+        backgroundColor: "rgba(16, 16, 16, 0.60)",
+        "& .MuiDialog-paper": {
+          backgroundColor: "#101010",
+          // // width: "64rem",
+          width: fullWidth ? "100%" : "auto",
+          maxWidth: "120rem",
+          padding: "3rem 3.2rem 3.2rem",
+          borderRadius: "1.6rem",
+        },
+      }}
+      onClose={onClose}
+      {...restProps}
+    >
+      <DialogTitle sx={{ padding: 0, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        {allowBack && (
+          <IconButton sx={{ p: 0, "&:hover": { backgroundColor: "unset" } }} onClick={onBack}>
+            <SvgIcon
+              sx={{ fontSize: ["1.6rem", "1.8rem"], color: "primary.contrastText", marginRight: "1.2rem" }}
+              component={BackSvg}
+              inheritViewBox
+            ></SvgIcon>
+            <Typography component="span" sx={{ fontSize: "1.8rem", color: "primary.contrastText", cursor: "pointer" }}>
+              Back
+            </Typography>
+          </IconButton>
+        )}
+
+        <Typography sx={{ fontSize: "3.2rem", lineHeight: "5.6rem", color: "primary.contrastText", fontWeight: 600 }}>{title}</Typography>
+        <IconButton sx={{ marginRight: "-0.8rem", "&:hover": { backgroundColor: "unset" } }} onClick={onClose}>
+          <SvgIcon sx={{ fontSize: ["1.6rem", "2.4rem"], color: "primary.contrastText" }} component={CloseSvg} inheritViewBox></SvgIcon>
+        </IconButton>
+      </DialogTitle>
+      {/* <Stack direction="row" alignItems="center" sx={{ position: "absolute" }}></Stack> */}
+      <DialogContent sx={{ padding: 0 }}>{children}</DialogContent>
+      {extra}
+    </Dialog>
+  )
+}
+
+export default ScrollDialog
