5740a6d1ac5f43107761e3a6a8cbb586ef49afcb
holybasil <holybasil.1128@gmail.com>
2024-04-11 22:04:19 +0800

    fix: check eligibility && get recipient by badgeId

--- src/pages/canvas/mint/flow/FirstBadgeStep.tsx
+++ src/pages/canvas/mint/flow/FirstBadgeStep.tsx
@@ -1,13 +1,16 @@
 import Img from "react-cool-img"
 
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, Stack, SvgIcon, Typography } from "@mui/material"
 
+import { ReactComponent as ValidSvg } from "@/assets/svgs/canvas/check.svg"
+import { ReactComponent as WarningSvg } from "@/assets/svgs/canvas/circle-warning.svg"
 import Button from "@/components/Button"
 import TextButton from "@/components/TextButton"
 import { ETHEREUM_YEAR_BADGE } from "@/constants/badges"
 import { useRainbowContext } from "@/contexts/RainbowProvider"
+import { useAsyncMemo } from "@/hooks"
 import useSnackbar from "@/hooks/useSnackbar"
-import { mintBadge } from "@/services/canvasService"
+import { checkBadgeEligibility, mintBadge } from "@/services/canvasService"
 import useCanvasStore from "@/stores/canvasStore"
 import { isUserRejected } from "@/utils"
 
@@ -17,6 +20,11 @@ const FirstBadgeStep = props => {
   const { provider, walletCurrentAddress } = useRainbowContext()
   const { isFirstBadgeMinting, changeIsFirstBadgeMinting, changeMintFlowVisible, queryVisibleBadges } = useCanvasStore()
   const alertWarning = useSnackbar()
+
+  const isEligible = useAsyncMemo(async () => {
+    const eligibility = await checkBadgeEligibility(provider, walletCurrentAddress, ETHEREUM_YEAR_BADGE)
+    return eligibility
+  }, [walletCurrentAddress, provider])
 
   const handleMintBadge = async () => {
     changeIsFirstBadgeMinting(true)
@@ -49,10 +57,28 @@ const FirstBadgeStep = props => {
         </>
       }
       action={
-        <Stack direction="column" gap="2.4rem">
-          <Button color="primary" width="28.2rem" loading={isFirstBadgeMinting} onClick={handleMintBadge}>
+        <Stack direction="column" gap="2.4rem" alignItems="center">
+          <Button color="primary" width="28.2rem" gloomy={!isEligible} loading={isFirstBadgeMinting} onClick={handleMintBadge}>
             {isFirstBadgeMinting ? "Minting badge" : "Mint badge"}
           </Button>
+          <Stack direction="row" gap="0.8rem" alignItems="center">
+            {isEligible ? (
+              <>
+                <SvgIcon sx={{ color: "#85E0D1", fontSize: "2.4rem" }} component={ValidSvg} inheritViewBox></SvgIcon>
+                <Typography sx={{ color: "#85E0D1 !important", fontSize: "1.8rem", lineHeight: "2.8rem", fontWeight: 500 }}>
+                  You are eligible to mint the badge
+                </Typography>
+              </>
+            ) : (
+              <>
+                <SvgIcon sx={{ color: "primary.main", fontSize: "2.4rem" }} component={WarningSvg} inheritViewBox></SvgIcon>
+                <Typography sx={{ color: "#FF684B !important", fontSize: "1.8rem", lineHeight: "2.8rem", fontWeight: 500 }}>
+                  Selected account is not eligible to mint the badge yet.
+                </Typography>
+              </>
+            )}
+          </Stack>
+
           <TextButton underline="always" sx={{ color: "#A0A0A0 !important", fontSize: "2rem", lineHeight: "3.5rem" }} onClick={handleViewMyCanvas}>
             Skip and go to my Canvas
           </TextButton>
