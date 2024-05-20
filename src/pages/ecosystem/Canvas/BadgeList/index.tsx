b04a8a309138ddd7d9e5492d4fa94a060b1aa353
holybasil <holybasil.1128@gmail.com>
2024-05-20 11:23:35 +0800

    fix: mobile ecosystem badges

--- src/pages/ecosystem/Canvas/BadgeList/index.tsx
+++ src/pages/ecosystem/Canvas/BadgeList/index.tsx
@@ -6,6 +6,7 @@ import { styled } from "@mui/system"
 
 import { ReactComponent as LeftSvg } from "@/assets/svgs/canvas/arrow-left.svg"
 import { ReactComponent as RightSvg } from "@/assets/svgs/canvas/arrow-right.svg"
+import useCheckViewport from "@/hooks/useCheckViewport"
 import { Badge } from "@/pages/canvas/Dashboard/UpgradeDialog/Badges"
 
 interface CarouselProps {
@@ -24,6 +25,13 @@ const BadgeCard = styled(Card)(({ theme }) => ({
   cursor: "pointer",
   "&  *": {
     cursor: "pointer !important",
+  },
+  [theme.breakpoints.down("md")]: {
+    width: "39.8rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "27.2rem",
+    height: "30.4rem",
   },
 }))
 
@@ -46,6 +54,7 @@ const ArrorButton = styled(IconButton)(({ theme }) => ({
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const { isLandscape, isMobile } = useCheckViewport()
   const navigate = useNavigate()
 
   const handlePrev = () => {
@@ -75,32 +84,39 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
       <Stack
         direction="row"
         justifyContent="space-between"
-        spacing="3rem"
-        sx={{ margin: "5rem 0" }}
+        sx={[
+          { margin: ["1.6rem 0 3rem", "5rem 0"], gap: ["0.8rem", "1.6rem", "3rem"] },
+          theme => ({
+            [theme.breakpoints.down("md")]: {
+              overflowX: "auto",
+              paddingBottom: "1rem",
+            },
+          }),
+        ]}
         style={{ transform: translateX, transition: "transform 0.5s ease" }}
       >
         {items.map((item, index) => (
           <BadgeCard onClick={e => handleCardClick(e, item)} key={index}>
             <CardContent
               sx={{
-                padding: "3rem 3rem 4rem !important",
+                padding: ["2.4rem 1.6rem !important", "3rem 3rem 4rem !important"],
                 height: "100%",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                gap: "2.4rem",
+                gap: "1.6rem",
               }}
             >
-              <img alt="logo" src={item.image} style={{ height: "8.8rem", borderRadius: "0.8rem" }} />
-              <Typography variant="h5" component="div">
+              <img alt="logo" src={item.image} style={{ height: isMobile ? "4.8rem" : "8.8rem", borderRadius: "0.8rem" }} />
+              <Typography sx={{ fontSize: ["2rem", "2.4rem"], lineHeight: "3.2rem", fontWeight: 600 }} component="div">
                 {item.name}
               </Typography>
               <Typography
-                variant="h6"
                 sx={{
+                  fontSize: ["1.6rem", "2rem"],
                   fontWeight: 400,
-                  lineHeight: "3.2rem",
-                  mb: "1.2rem",
+                  lineHeight: ["2.4rem", "3.2rem"],
+                  // mb: [0, "1.2rem"],
                   flex: 1,
 
                   display: "-webkit-box",
@@ -112,22 +128,28 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
               >
                 {item.description}
               </Typography>
-              <Typography variant="h5">
-                <img alt="issuer logo" src={item.issuer.logo} style={{ height: "4.8rem", borderRadius: "0.5rem", marginRight: "1.4rem" }} />
+              <Typography sx={{ fontSize: ["1.8rem", "2.4rem"], lineHeight: ["2.8rem", "3.2rem"], fontWeight: 600, mt: [0, "2.8rem"] }}>
+                <img
+                  alt="issuer logo"
+                  src={item.issuer.logo}
+                  style={{ height: isMobile ? "3.2rem" : "4.8rem", borderRadius: "0.5rem", marginRight: "1.4rem" }}
+                />
                 {item.issuer.name}
               </Typography>
             </CardContent>
           </BadgeCard>
         ))}
       </Stack>
-      <Box display="flex" justifyContent="flex-end" gap={"1.6rem"} marginTop={2} paddingBottom="9.5rem">
-        <ArrorButton onClick={handlePrev} disabled={isPrevDisabled} aria-label="previous">
-          <SvgIcon component={LeftSvg} inheritViewBox />
-        </ArrorButton>
-        <ArrorButton onClick={handleNext} disabled={isNextDisabled} aria-label="next">
-          <SvgIcon component={RightSvg} sx={{ fontSize: "2.2rem" }} inheritViewBox />
-        </ArrorButton>
-      </Box>
+      {isLandscape && (
+        <Box display="flex" justifyContent="flex-end" gap={"1.6rem"} marginTop={2} paddingBottom="9.5rem">
+          <ArrorButton onClick={handlePrev} disabled={isPrevDisabled} aria-label="previous">
+            <SvgIcon sx={{ fontSize: "1.6rem" }} component={LeftSvg} inheritViewBox />
+          </ArrorButton>
+          <ArrorButton onClick={handleNext} disabled={isNextDisabled} aria-label="next">
+            <SvgIcon sx={{ fontSize: "1.6rem" }} component={RightSvg} inheritViewBox />
+          </ArrorButton>
+        </Box>
+      )}
     </Box>
   )
 }
