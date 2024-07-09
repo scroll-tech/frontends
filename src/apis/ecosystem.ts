990af4958f9bbda1151c09d6aae695912c3452bb
yqrashawn <namy.19@gmail.com>
2024-07-09 14:18:39 +0800

    feat: vercel.app -> scroll.cat (#1172)

--- src/apis/ecosystem.ts
+++ src/apis/ecosystem.ts
@@ -1,7 +1,7 @@
 export const ecosystemOrigin = "https://scroll-eco-list.netlify.app"
 export const ecosystemListHashUrl = ecosystemOrigin + "/docs/"
 export const ecosystemListLogoUrl = ecosystemOrigin + "/logos/"
-export const ecosystemListUrl = "https://ecosystem-list-api.vercel.app/api/query"
+export const ecosystemListUrl = "https://ecosystem.scroll.cat/api/query"
 
 export const l2beatOrigin = "https://l2beat.com"
 // lack of the latest day
