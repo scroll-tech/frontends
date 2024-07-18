7df1f740d35a8478c7679b628affb85bec1f8671
holybasil <holybasil.1128@gmail.com>
2024-07-18 15:38:15 +0800

    fix: async name validation

--- src/hooks/useValidateCanvasName.tsx
+++ src/hooks/useValidateCanvasName.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { usePrevious } from "react-use"
 import { useDebouncedCallback } from "use-debounce"
+import { hexToBool } from "viem"
 
 import { CircularProgress, SvgIcon, Typography } from "@mui/material"
 
 import SensitiveWord from "@/assets/files/sensitive-word.json"
 import { ReactComponent as CheckSvg } from "@/assets/svgs/canvas/check.svg"
 import { ReactComponent as WarningSvg } from "@/assets/svgs/canvas/warning.svg"
+import { RPC_URL } from "@/constants"
 import { useCanvasContext } from "@/contexts/CanvasContextProvider"
 import useCanvasStore from "@/stores/canvasStore"
 
@@ -15,8 +17,9 @@ const useValidateName = value => {
   const { username } = useCanvasStore()
 
   const preValue = usePrevious(value)
-  const [helpText, setHelpText] = useState<string | null>(null)
+  const [helpText, setHelpText] = useState<string | JSX.Element | null>(null)
   const [validating, setValidating] = useState(false)
+  const controller = useRef<any>()
 
   useEffect(() => {
     if ((preValue && !value) || value !== username) {
@@ -26,7 +29,7 @@ const useValidateName = value => {
 
   const handleValidateName = useDebouncedCallback(async value => {
     setValidating(true)
-    const nextHelpText = await validateName(value)
+    const nextHelpText: string | JSX.Element = await validateName(value)
     setHelpText(nextHelpText)
     setValidating(false)
     return nextHelpText
@@ -37,26 +40,52 @@ const useValidateName = value => {
   }
 
   const validateName = async name => {
-    let nextHelpText
+    let helpText: string | JSX.Element = ""
+    if (controller.current) {
+      controller.current.abort("user input")
+    }
+
     if (!name) {
-      nextHelpText = "Please enter your name"
+      helpText = "Please enter your name"
     } else if (!/^[\dA-Za-z_]{4,15}$/g.test(name)) {
-      nextHelpText = (
+      helpText = (
         <>
           Your name must consist of 4 to 15 characters,<br></br>comprising only letters, numbers or underline.
         </>
       )
     } else if (SensitiveWord.some(word => name.toLowerCase().includes(word.toLowerCase()))) {
-      nextHelpText = "This name is not allowed"
+      helpText = "This name is not allowed" as string
     } else {
-      const isUsernameUsed = await unsignedProfileRegistryContract.isUsernameUsed(name)
+      // const isUsernameUsedCallData = encodeFunctionData({
+      //   abi: ProfileRegistryABI,
+      //   functionName: "isUsernameUsed",
+      //   args: [name],
+      // })
+      // const callParams = {
+      //   to: requireEnv("REACT_APP_PROFILE_REGISTRY_ADDRESS"),
+      //   data: isUsernameUsedCallData,
+      // }
+      //
+      // TODO: viem will implement request cancellation
+      const callParams = await unsignedProfileRegistryContract.isUsernameUsed.populateTransaction(name)
+      const data = {
+        jsonrpc: "2.0",
+        method: "eth_call",
+        params: [callParams, "latest"],
+        id: 123,
+      }
+      controller.current = new AbortController()
+      const result = await scrollRequest(RPC_URL.L2, {
+        method: "POST",
+        body: JSON.stringify(data),
+        signal: controller.current.signal,
+      })
+      const isUsernameUsed = hexToBool(result.result)
       if (isUsernameUsed) {
-        nextHelpText = "This name is already taken"
-      } else {
-        nextHelpText = ""
+        helpText = "This name is already taken"
       }
     }
-    return nextHelpText
+    return helpText
   }
 
   const renderValidation = () => {
