ee8ed96ee0274f1f80621a5b482c9761ca2cc07a
zhengqi zhang <770166635@qq.com>
2024-08-05 09:56:24 +0800

    Remove duplicate category

--- src/constants/ecosystem.ts
+++ src/constants/ecosystem.ts
@@ -8,7 +8,7 @@ export const DIVERGENT_CATEGORY_MAP = {
   Community: ["Community", "DAO", "Governance"],
   DeFi: ["DEX", "DeFi", "Launchpad", "Lending", "Marketplace", "Payment"],
   Gaming: ["Gaming"],
-  Infra: ["Bridge", "Gateway", "Indexer", "Infrastructure", "Node Provider", "Oracle"],
+  Infra: ["Gateway", "Indexer", "Infrastructure", "Node Provider", "Oracle"],
   NFT: ["NFT"],
   Privacy: ["Privacy", "Identity"],
   Social: ["Social"],
