75646cb6490109bd79abb7f52f268fb701f3d926
holybasil <holybasil.1128@gmail.com>
2024-08-15 15:53:35 +0800

    fix: badge&star flicker

--- src/pages/canvas-badge/Header/index.tsx
+++ src/pages/canvas-badge/Header/index.tsx
@@ -1,116 +1,20 @@
 import { useQuery } from "@tanstack/react-query"
-import { motion, useTime, useTransform } from "framer-motion"
+import { motion } from "framer-motion"
 import { useEffect, useRef, useState } from "react"
 import Img from "react-cool-img"
 
-import { Box, Container, Stack, Typography } from "@mui/material"
+import { Box, Container, Stack, SvgIcon, Typography } from "@mui/material"
 import { styled } from "@mui/material/styles"
 
 import { retrieveCanvasBadgeURL } from "@/apis/canvas-badge"
 import Button from "@/components/Button"
-import { CANVAS_URL, ISSUE_BADGES_URL } from "@/constants"
+import { CANVAS_URL, HEADER_BADGES, HEADER_STARS, ISSUE_BADGES_URL } from "@/constants"
 import useCheckViewport from "@/hooks/useCheckViewport"
 
 import Counter from "./Counter"
 import Statistic from "./Statistic"
 
-const AnimatedContainer = motion(Container)
-
 const ImageWrapper = motion(Box)
-
-// wrapper 1328x337
-const BADGES = [
-  {
-    image: "/imgs/canvas-badge/Ambient.webp",
-    key: "Ambient",
-    width: "13.9rem",
-    top: 0,
-  },
-  {
-    image: "/imgs/canvas-badge/Zebra.webp",
-    key: "Zebra",
-    width: "18.8rem",
-    top: "13.5rem",
-    left: "9.55rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Panda.png",
-    key: "Panda",
-    width: "13.5rem",
-    top: 0,
-    left: "17.4rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Pencils.png",
-    key: "Pencils",
-    width: "16.9rem",
-    top: "8.2rem",
-    left: "27rem",
-  },
-  {
-    image: "/imgs/canvas-badge/EAS.png",
-    key: "EAS",
-    width: "20.3rem",
-    top: "13.5rem",
-    left: "41.8rem",
-  },
-  {
-    image: "/imgs/canvas-badge/SymTrader.png",
-    key: "SymTrader",
-    width: "13.5rem",
-    top: 0,
-    left: "45.9rem",
-  },
-  {
-    image: "/imgs/canvas/Badge_Ethereum_Year.png",
-    key: "Ethererum",
-    width: "16.9rem",
-    top: "6.75rem",
-    left: "59.45rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Passport.png",
-    key: "Passport",
-    width: "20.3rem",
-    top: "13.5rem",
-    left: "73.1rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Cog.png",
-    key: "Cog",
-    width: "12.5rem",
-    top: 0,
-    left: "78.7rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Scrolly.png",
-    key: "Scrolly",
-    width: "13.5rem",
-    top: 0,
-    left: "101.9rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Scroller.png",
-    key: "Scroller",
-    width: "16.8rem",
-    top: "8.2rem",
-    left: "92.8rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Flock-x.png",
-    key: "Flock",
-    width: "20.2rem",
-    top: "13.5rem",
-    left: "106.4rem",
-  },
-  {
-    image: "/imgs/canvas-badge/Symbiosis.png",
-    key: "Symbiosis",
-    width: "13.8rem",
-    top: 0,
-    left: "119rem",
-  },
-]
 
 const BadgesButton = styled(Button)(({ theme }) => ({
   backgroundColor: theme.palette.themeBackground.dark,
@@ -119,34 +23,12 @@ const BadgesButton = styled(Button)(({ theme }) => ({
   },
 }))
 
-function pickRandomNumbers(excludeNumbers: number[] = []) {
-  const count = Math.random() < 0.5 ? 3 : 4
-  const numbers = Array.from({ length: BADGES.length }, (_, i) => i).filter(item => !excludeNumbers.includes(item))
-
-  for (let i = numbers.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[numbers[i], numbers[j]] = [numbers[j], numbers[i]]
-  }
-
-  return numbers.slice(0, count)
-}
-
 const Header = () => {
   const { isMobile } = useCheckViewport()
   const [badgesScale, setBadgesScale] = useState(1)
+  const [starsScale, setStarsScale] = useState(1)
 
-  const [randomNumbers, setRandomNumbers] = useState(pickRandomNumbers())
   const badgesContainerRef = useRef<any>()
-
-  const time = useTime()
-  const transform = useTransform(time, value => Math.floor(value / 1000))
-  useEffect(() => {
-    const unsubscribe = transform.onChange(() => {
-      setRandomNumbers(pre => pickRandomNumbers(pre))
-    })
-
-    return () => unsubscribe()
-  }, [transform])
 
   const { data } = useQuery({
     queryKey: ["totalCount"],
@@ -171,6 +53,11 @@ const Header = () => {
       } else {
         setBadgesScale(1)
       }
+      if (window.innerWidth < 1512) {
+        setStarsScale((window.innerWidth - 120) / 1512)
+      } else {
+        setStarsScale(1)
+      }
     }
     handleWindowResize()
 
@@ -185,14 +72,14 @@ const Header = () => {
     <Box
       sx={[
         {
+          position: "relative",
           width: "100%",
           aspectRatio: ["unset", "unset", "1512 / 850"],
           height: "52vw",
           backgroundSize: "auto 100%",
-          position: "relative",
           textAlign: "center",
           backgroundColor: "themeBackground.dark",
-          background: "url(/imgs/canvas-badge/header_bg.webp) center bottom / 100% no-repeat",
+          background: "url(/imgs/canvas-badge/badges-header-bg.webp) center bottom / 100% no-repeat",
           pt: ["5.6rem", "6.4vw"],
           px: ["2rem"],
           "& p": {
@@ -216,6 +103,29 @@ const Header = () => {
         }),
       ]}
     >
+      <Container
+        sx={{
+          position: "absolute",
+          top: ["40rem", "4vw"],
+          left: "50%",
+          transform: `translateX(calc(${starsScale < 1 ? "6rem - 50%" : "-50%"})) scale(${starsScale})`,
+          transformOrigin: "top left",
+        }}
+      >
+        {HEADER_STARS.map(({ icon, size, left, top }) => (
+          <ImageWrapper
+            animate={{ opacity: [0.2, 1, 0.2] }}
+            transition={{
+              duration: 3,
+              repeat: Infinity,
+              delay: Math.random() * 3,
+              times: [0, 0.5, 1],
+            }}
+          >
+            <SvgIcon component={icon} sx={{ fontSize: size, position: "absolute", top, left }} inheritViewBox></SvgIcon>
+          </ImageWrapper>
+        ))}
+      </Container>
       <Typography sx={{ fontSize: ["3.8rem", "7.8rem"], lineHeight: ["5.6rem", "8.8rem"], fontWeight: 600, mb: [0, "1.6rem"] }}>
         Canvas and Badges
       </Typography>
@@ -240,7 +150,7 @@ const Header = () => {
           </BadgesButton>
         </Stack>
       </Stack>
-      <AnimatedContainer sx={{ mt: ["9.5rem", "6rem", "2.8rem"], px: "0 !important" }} ref={badgesContainerRef}>
+      <Box sx={{ mt: ["9.5rem", "6rem", "2.8rem"] }} ref={badgesContainerRef}>
         <Box
           sx={{
             position: "relative",
@@ -251,15 +161,15 @@ const Header = () => {
             transformOrigin: "top left",
           }}
         >
-          {BADGES.map(({ image, top, left, width }, index) => (
+          {HEADER_BADGES.map(({ image, top, left, width }, index) => (
             <ImageWrapper
-              style={{ willChange: "opacity", zIndex: randomNumbers.includes(index) ? 1 : 0 }}
-              animate={{ opacity: randomNumbers.includes(index) ? [0.2, 1] : [0.2, 0.2] }}
+              style={{ willChange: "opacity", zIndex: 1 }}
+              animate={{ opacity: [0.2, 1, 0.2] }}
               transition={{
-                duration: 1,
-                ease: "easeOut",
+                duration: 3,
                 repeat: Infinity,
-                // repeatDelay: 2,
+                delay: Math.random() * 3,
+                times: [0, 0.5, 1],
               }}
               sx={{ position: "absolute", top, left }}
             >
@@ -267,7 +177,7 @@ const Header = () => {
             </ImageWrapper>
           ))}
         </Box>
-      </AnimatedContainer>
+      </Box>
     </Box>
   )
 }
