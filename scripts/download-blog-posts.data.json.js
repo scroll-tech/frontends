8990b28d1998dcb2f5f0c136904fa243392f59b7
yqrashawn <namy.19@gmail.com>
2024-08-27 10:07:47 +0800

    chore(blog): fetch blog/data.json with origin

--- scripts/download-blog-posts.data.json.js
+++ scripts/download-blog-posts.data.json.js
@@ -5,7 +5,7 @@ const isMainnet = process.env.REACT_APP_SCROLL_ENVIRONMENT === "Mainnet"
 const POSTS_URL = `https://blog.scroll.cat/api/posts/${isMainnet ? "published" : "preview"}/data.json`
 
 async function fetchPosts() {
-  await fetch(POSTS_URL)
+  await fetch(POSTS_URL, { headers: { origin: "https://scroll.io" } })
     .then(res => res.json())
     .then(json => fs.writeFileSync("./src/pages/blog/data.json", JSON.stringify(json, null, 2)))
 }
