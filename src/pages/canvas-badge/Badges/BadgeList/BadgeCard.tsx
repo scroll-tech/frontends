7bb080364fd593fd40185380235d6228f8e58d67
holybasil <holybasil.1128@gmail.com>
2024-08-29 09:26:39 +0800

    fix: allow new tab

--- src/pages/canvas-badge/Badges/BadgeList/BadgeCard.tsx
+++ src/pages/canvas-badge/Badges/BadgeList/BadgeCard.tsx
@@ -1,4 +1,5 @@
 import Img from "react-cool-img"
+import { Link } from "react-router-dom"
 import { makeStyles } from "tss-react/mui"
 
 import { Box, Card, CardContent, SvgIcon, Typography } from "@mui/material"
@@ -36,96 +37,97 @@ const useStyles = makeStyles()(theme => ({
 }))
 
 const BadgeCard = props => {
-  const { name, image, description, issuer, category, count, className, onClick } = props
+  const { name, image, description, badgeContract, issuer, category, count, className } = props
   const { classes } = useStyles()
   const { isMobile } = useCheckViewport()
 
   return (
-    <Card
-      sx={{
-        position: "relative",
-        aspectRatio: ["330 / 320", "330 / 336"],
-        borderRadius: "2rem",
-        backgroundColor: "themeBackground.tag",
-        boxShadow: "none",
-        border: "none",
-        cursor: "pointer",
-        "& p": {
-          cursor: "pointer !important",
-        },
-        "&:hover": {
-          backgroundColor: "#323232",
-        },
-      }}
-      className={className}
-      onClick={onClick}
-    >
-      <CardContent
+    <Link to={`/canvas/badge-contract/${badgeContract}`}>
+      <Card
         sx={{
-          padding: ["1.6rem !important", "2.4rem !important"],
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "0.8rem",
-
+          position: "relative",
+          aspectRatio: ["330 / 320", "330 / 336"],
+          borderRadius: "2rem",
+          backgroundColor: "themeBackground.tag",
+          boxShadow: "none",
+          border: "none",
+          cursor: "pointer",
           "& p": {
-            color: "primary.contrastText",
+            cursor: "pointer !important",
+          },
+          "&:hover": {
+            backgroundColor: "#323232",
           },
         }}
+        className={className}
       >
-        {category === CATEGORY_LIST[1].key && <Box className={classes.category}>{CATEGORY_LIST[1].label}</Box>}
-        {category === CATEGORY_LIST[2].key && <Box className={classes.category}>{CATEGORY_LIST[2].label}</Box>}
-        {category === CATEGORY_LIST[3].key && <Box className={classes.category}>{CATEGORY_LIST[3].label}</Box>}
-        <Img
-          alt="logo"
-          src={image}
-          placeholder="/imgs/canvas-badge/badgePlaceholder.svg"
-          style={{ height: isMobile ? "6.4rem" : "8rem", aspectRatio: "1 / 1", borderRadius: "0.45rem" }}
-        />
-        <Typography
-          sx={[{ fontSize: ["1.8rem", "2rem"], lineHeight: "3.2rem", fontWeight: 600, maxWidth: "100%" }, theme => theme.singleLineEllipsis]}
-        >
-          {name}
-        </Typography>
-
-        <Typography sx={{ fontSize: ["1.4rem", "1.6rem"], lineHeight: "2.4rem", fontWeight: 500, fontFamily: "var(--developer-page-font-family)" }}>
-          <SvgIcon sx={{ fontSize: "0.6rem", mr: "0.8rem" }} component={TotalMintedSvg} inheritViewBox></SvgIcon>
-          Total Minted: {commafy(count, 0)}
-        </Typography>
-        <Typography
+        <CardContent
           sx={{
-            fontSize: ["1.6rem", "1.6rem"],
-            fontWeight: 400,
-            lineHeight: ["2.4rem", "2.4rem"],
-            flex: 1,
-            display: "-webkit-box",
-            width: "100%",
-            textAlign: "center",
-            WebkitBoxOrient: "vertical",
-            WebkitLineClamp: ["4", "3"],
-            overflow: "hidden",
+            padding: ["1.6rem !important", "2.4rem !important"],
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "0.8rem",
+
+            "& p": {
+              color: "primary.contrastText",
+            },
           }}
         >
-          <BadgeDesc>{description}</BadgeDesc>
-        </Typography>
-        <Typography sx={{ fontSize: "1.8rem", lineHeight: ["2.8rem", "3.2rem"], fontWeight: 600, mt: ["0.8rem", "1.6rem"] }}>
+          {category === CATEGORY_LIST[1].key && <Box className={classes.category}>{CATEGORY_LIST[1].label}</Box>}
+          {category === CATEGORY_LIST[2].key && <Box className={classes.category}>{CATEGORY_LIST[2].label}</Box>}
+          {category === CATEGORY_LIST[3].key && <Box className={classes.category}>{CATEGORY_LIST[3].label}</Box>}
           <Img
-            alt={issuer?.name}
-            src={issuer?.logo}
-            style={{
-              backgroundColor: "white",
-              width: isMobile ? "3.2rem" : "3.2rem",
-              height: isMobile ? "3.2rem" : "3.2rem",
-              borderRadius: "0.45rem",
-              marginRight: "1.375rem",
-              verticalAlign: "top",
-            }}
+            alt="logo"
+            src={image}
+            placeholder="/imgs/canvas-badge/badgePlaceholder.svg"
+            style={{ height: isMobile ? "6.4rem" : "8rem", aspectRatio: "1 / 1", borderRadius: "0.45rem" }}
           />
-          {issuer?.name}
-        </Typography>
-      </CardContent>
-    </Card>
+          <Typography
+            sx={[{ fontSize: ["1.8rem", "2rem"], lineHeight: "3.2rem", fontWeight: 600, maxWidth: "100%" }, theme => theme.singleLineEllipsis]}
+          >
+            {name}
+          </Typography>
+
+          <Typography sx={{ fontSize: ["1.4rem", "1.6rem"], lineHeight: "2.4rem", fontWeight: 500, fontFamily: "var(--developer-page-font-family)" }}>
+            <SvgIcon sx={{ fontSize: "0.6rem", mr: "0.8rem" }} component={TotalMintedSvg} inheritViewBox></SvgIcon>
+            Total Minted: {commafy(count, 0)}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: ["1.6rem", "1.6rem"],
+              fontWeight: 400,
+              lineHeight: ["2.4rem", "2.4rem"],
+              flex: 1,
+              display: "-webkit-box",
+              width: "100%",
+              textAlign: "center",
+              WebkitBoxOrient: "vertical",
+              WebkitLineClamp: ["4", "3"],
+              overflow: "hidden",
+            }}
+          >
+            <BadgeDesc>{description}</BadgeDesc>
+          </Typography>
+          <Typography sx={{ fontSize: "1.8rem", lineHeight: ["2.8rem", "3.2rem"], fontWeight: 600, mt: ["0.8rem", "1.6rem"] }}>
+            <Img
+              alt={issuer?.name}
+              src={issuer?.logo}
+              style={{
+                backgroundColor: "white",
+                width: isMobile ? "3.2rem" : "3.2rem",
+                height: isMobile ? "3.2rem" : "3.2rem",
+                borderRadius: "0.45rem",
+                marginRight: "1.375rem",
+                verticalAlign: "top",
+              }}
+            />
+            {issuer?.name}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   )
 }
 export default BadgeCard
