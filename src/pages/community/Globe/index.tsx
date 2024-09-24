0e5fa2700681e261cbac267d71f0cf4e79b6941b
zzq0826 <770166635@qq.com>
2024-09-24 15:18:10 +0800

    Adjust globe json path (#1287)
    Co-authored-by: Sourav Gupta <zhangzhengqi0826@gmail.com>

--- src/pages/community/Globe/index.tsx
+++ src/pages/community/Globe/index.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { Suspense, lazy } from "react"
 import { makeStyles } from "tss-react/mui"
 
 import { Box, Typography } from "@mui/material"
 
-import Data from "./data.json"
-import { initCityLabels, renderer, resizeRender } from "./mainscene/scene"
+const GlobeComponent = lazy(() => import("./GlobeComponent"))
 
 const useStyles = makeStyles()(theme => ({
   title: {
@@ -25,53 +24,15 @@ const useStyles = makeStyles()(theme => ({
   },
 }))
 
-const MAX_SIZE = 700
-
 function Earth() {
-  const threeDomRef = useRef<HTMLDivElement | null>(null)
-  const [size, setSize] = useState(Math.min(window.innerWidth - 16, MAX_SIZE))
   const { classes } = useStyles()
-
-  useEffect(() => {
-    const handleResize = () => {
-      const newSize = Math.min(window.innerWidth - 16, MAX_SIZE)
-      setSize(newSize)
-      if (threeDomRef.current) {
-        const { left, top } = threeDomRef.current.getBoundingClientRect()
-        resizeRender(newSize, newSize, left, top)
-      }
-    }
-
-    window.addEventListener("resize", handleResize)
-    handleResize()
-
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (threeDomRef.current) {
-      threeDomRef.current.appendChild(renderer.domElement)
-      const { left, top } = threeDomRef.current.getBoundingClientRect()
-      resizeRender(size, size, left, top)
-      initCityLabels(Data)
-    }
-  }, [size])
 
   return (
     <Box>
       <Typography className={classes.title}>Scroll Around the World</Typography>
-      <Box sx={{ display: "flex", height: size + "px", justifyContent: "center" }}>
-        <Box
-          ref={threeDomRef}
-          style={{
-            width: `${size}px`,
-            height: `${size}px`,
-            position: "absolute",
-          }}
-        ></Box>
-      </Box>
+      <Suspense fallback={<div style={{ height: "70rem" }}></div>}>
+        <GlobeComponent />
+      </Suspense>
     </Box>
   )
 }
