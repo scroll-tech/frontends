8ccac708f543a9dc9be7f7042c18e76867924b0b
holybasil <holybasil.1128@gmail.com>
2025-01-08 17:56:35 +0800

    fix: svg && metadata

--- src/app/builder-portal/page.tsx
+++ src/app/builder-portal/page.tsx
@@ -1,5 +1,13 @@
+import { genMeta } from "@/utils/route"
+
 import Content from "./Content"
 import Header from "./Header"
+
+export const generateMetadata = genMeta(() => ({
+  titleSuffix: "Builder Portal",
+  relativeURL: "/build-portal",
+  description: "Your all-in-one resource hub for building on Scroll.",
+}))
 
 const BuilderPortalPage = () => {
   return (
