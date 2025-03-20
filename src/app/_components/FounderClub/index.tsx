77932a67f194c6f2e32b4bea29d656935c2786cf
holybasil <holybasil.1128@gmail.com>
2025-03-20 19:37:06 +0800

    fix: founder club

--- src/app/_components/FounderClub/index.tsx
+++ src/app/_components/FounderClub/index.tsx
@@ -1,20 +1,22 @@
 import { Box, Container, Typography } from "@mui/material"
 
 import Button from "@/components/Button"
+import { FOUNDER_CLUB_URL } from "@/constants/link"
 
+import Founders from "./Founders"
 import Protocols from "./Protocols"
 
 const FounderClub = () => {
   return (
     <Box sx={{ backgroundColor: "background.default", py: ["5.6rem", "8.9rem"] }}>
-      <Typography sx={{ fontSize: ["2.8rem", "3.6rem"], lineHeight: ["4.4rem", "5.2rem"], textAlign: "center", typography: "title" }}>
+      <Typography sx={{ fontSize: ["2.8rem", "3.6rem"], lineHeight: ["4.4rem", "5.2rem"], textAlign: "center", typography: "title", px: "2rem" }}>
         Scale on a performant network
         <br className="hidden sm:inline" /> built for founders
       </Typography>
       <Protocols />
       <Container>
-        <Box sx={{ mt: ["3.2rem", "8.1rem"], mb: ["4rem", "8.1rem"] }}>TODO</Box>
-        <Button color="primary" className="!w-[240px] md:!w-[250px] md:mx-auto">
+        <Founders />
+        <Button color="primary" href={FOUNDER_CLUB_URL} className="!w-[240px] md:!w-[250px] md:mx-auto">
           Join Founders’ club
         </Button>
       </Container>
