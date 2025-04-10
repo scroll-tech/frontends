69ef9b9eb4fed77a5277624c80d03936bed0900f
holybasil <holybasil.1128@gmail.com>
2025-04-10 09:20:07 +0800

    chore: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -82,6 +82,17 @@ const nextConfig = {
 
       { source: "/sticker-vote", destination: "/sticker-winners", permanent: true },
       // { source: "/airdrop-faq", destination: "https://scroll-faqs.gitbook.io/faqs", permanent: false},
+
+      {
+        source: '/x',
+        destination: 'https://scroll.io?utm_source=x&utm_medium=social',
+        permanent: false,
+      },
+      {
+        source: '/l',
+        destination: 'https://scroll.io?utm_source=linkedin&utm_medium=social',
+        permanent: false,
+      },
     ]
   },
   async rewrites() {
